9f312a7e83ab7d4dd85b75f122fbb86dbd4eceb1
chris <chris>
2004-10-18 09:44:21 +0000

    Special cases; test data.

--- MaPit/MaPit.pm
+++ MaPit/MaPit.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: MaPit.pm,v 1.5 2004-10-18 09:24:56 francis Exp $
+# $Id: MaPit.pm,v 1.6 2004-10-18 09:44:21 chris Exp $
 #
 
 package MaPit;
@@ -37,6 +37,53 @@ sub dbh () {
     $dbh ||= DBI->connect('dbi:SQLite:dbname=/home/chris/projects/mysociety/MaPit/mapit.sqlite', '', '', { RaiseError => 1, AutoCommit => 0 });
     return $dbh;
 }
+
+# Special cases to represent parliaments, assemblies themselves.
+my %special_cases = (
+        # Enclosing bodies
+        900000 => {
+            type => mySociety::VotingArea::WMP,
+            name => 'House of Commons',
+        },
+        900001 => {
+            type => mySociety::VotingArea::EUP,
+            name => 'European Parliament',
+        },
+        
+        # Test data
+        1000001 => {
+            type => mySociety::VotingArea::CTY,
+            name => 'Cambridgeshire County Council'
+        },
+        1000002 => {
+            type => mySociety::VotingArea::CED,
+            name => 'West Chesterton ED'
+        },
+        1000003 => {
+            type => mySociety::VotingArea::DIS,
+            name => 'Cambridge District Council'
+        },
+        1000004 => {
+            type => mySociety::VotingArea::DIW,
+            name => 'West Chesterton Ward'
+        },
+        1000005 => {
+            type => mySociety::VotingArea::WMP,
+            name => 'House of Commons'
+        },
+        1000006 => {
+            type => mySociety::VotingArea::WMC,
+            name => 'Cambridge'
+        },
+        1000007 => {
+            type => mySociety::VotingArea::EUP,
+            name => 'European Parliament'
+        },
+        1000008 => {
+            type => mySociety::VotingArea::EUR,
+            name => 'Eastern Euro Region'
+        }
+    );
 
 =item get_voting_areas POSTCODE
 
@@ -71,10 +118,15 @@ sub get_voting_areas ($$) {
     my $pcid = dbh()->selectrow_array('select id from postcode where postcode = ?', {}, $pc);
     return mySociety::MaPit::POSTCODE_NOT_FOUND if (!$pcid);
 
+    # Also add pseudo-areas.
     return {
-            map { $_->[0] => $_->[1] } @{
+            (
+                map { $special_cases{$_}->{type} => $_ } keys %special_cases
+            ), (
+                map { $mySociety::VotingArea::type_to_id{$_->[0]} => $_->[1] } @{
                     dbh()->selectall_arrayref('select type, id from postcode_area, area where postcode_area.area_id = area.id and postcode_area.postcode_id = ?', {}, $pcid)
                 }
+            )
         };
 }
 
@@ -85,19 +137,8 @@ sub get_voting_area_info ($$) {
     my ($x, $id) = @_;
 
     my $ret;
-    if ($id >=1000001 && $id <=1000008) {
-        # Dummy postcode case
-        my %db = (
-                1000001 => {type => mySociety::VotingArea::CTY, name => 'Cambridgeshire County Council'},
-                1000002 => {type => mySociety::VotingArea::CED, name => 'West Chesterton ED'},
-                1000003 => {type => mySociety::VotingArea::DIS, name => 'Cambridge District Council'},
-                1000004 => {type => mySociety::VotingArea::DIW, name => 'West Chesterton Ward'},
-                1000005 => {type => mySociety::VotingArea::WMP, name => 'House of Commons'},
-                1000006 => {type => mySociety::VotingArea::WMC, name => 'Cambridge'},
-                1000007 => {type => mySociety::VotingArea::EUP, name => 'European Parliament'},
-                1000008 => {type => mySociety::VotingArea::EUR, name => 'Eastern Euro Region'}
-            );
-        $ret = $db{$id};
+    if (exists($special_cases{$id})) {
+        $ret = $special_cases{$id};
     } else {
         # Real data
         my ($type, $name);
