649962eca043ee6f389fa8270cf4091a1cb35069
francis <francis>
2005-12-08 12:40:41 +0000

    Was a bit out of date

--- ms/web/projects.php
+++ ms/web/projects.php
@@ -25,8 +25,7 @@ the UK General Election on May 5th 2005 had the chance to tell the
 world why. Non-judgmental and non-partisan NotApathetic caught the
 attention of 40,000 visitors during the general election, and was
 discussed on a dozen local radio stations, and in newspapers from New
-Zealand to South Africa. A dataset of categorized explanations will be
-made available from NotApathetic.com in the summer of 2005.
+Zealand to South Africa. 
 </div>
 <div class="item_inner_head">
 <a href="http://www.pledgebank.com">PledgeBank.com - Launched 13th June 2005</a>
@@ -37,6 +36,15 @@ socially beneficially that they won't be alone in their actions. It lets users
 create pledges which say "I'll do something, but only if 10 other people will
 do something", for example "I'll clean up the banks of my local river, but only
 if 5 other local people will pledge to come and help.
+</div>
+<div class="item_inner_head">
+<a href="http://www.hearfromyourmp.com">HearFromYourMP.com - Launched 21st November 2005</a>
+</div>
+<div class="item">
+HearFromYourMP encourages and enables MPs to run email lists for their
+constituents, and to allow those constituents to discuss ideas in a way which
+doesn't bombard them with email.  5000 people had signed up before it was even
+launched.
 </div>
 <div class="item">
 <strong>We appreciate feedback on all our live sites, so please tell us what we can improve.</strong>
@@ -52,16 +60,7 @@ Sites In Testing
 Sites Being Built
 </div>
 <div class="item_inner_head">
-<a href="/moin.cgi/YourConstituencyMailingList">Your Constituency Mailing List - now being built</a>
-</div>
-<div class="item">
-A site that encourages and enables MPs to run email lists for their
-constituents, and to allow those constituents to discuss ideas in a way which
-doesn't bombard them with email.  Even though the site hasn't been built
-yet, you can <a href="http://www.mysociety.org/ycml/">already sign up</a>.
-</div>
-<div class="item_inner_head">
-<a href="/moin.cgi/GiveItAway">Give It Away - to be built later in 2005</a>
+<a href="/moin.cgi/GiveItAway">Give It Away - to be built late 2005 / early 2006</a>
 </div>
 <div class="item">
 A site to make it as easy to give something unwanted away to charity as it is to throw it away.
