eae6a21ef6d24586c54272f1fd47f3fef358317f
chris <chris>
2006-01-09 14:09:52 +0000

    "When" is a reserved word.

--- ms/db/cvstrac-schema-changes.sql
+++ ms/db/cvstrac-schema-changes.sql
@@ -5,12 +5,12 @@
 -- Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 -- Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: cvstrac-schema-changes.sql,v 1.1 2006-01-09 14:05:51 chris Exp $
+-- $Id: cvstrac-schema-changes.sql,v 1.2 2006-01-09 14:09:52 chris Exp $
 --
 
 create table volunteer_interest (
     ticket_num integer not null references ticket(tn),
     name text not null,
     email text not null,
-    when integer not null
+    whenregistered integer not null
 );
