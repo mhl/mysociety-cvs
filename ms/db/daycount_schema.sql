4bcb3e6b0a73a971749767eea398f61cf66e3550
louise <louise>
2006-02-22 18:54:56 +0000

    initial checkin of daycount schema

---
+++ ms/db/daycount_schema.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+-- schema.sql:
+-- Schema for meetup day preference app (daycount)
+--
+-- Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+-- Email: louise.crow@gmail.com; WWW: http://www.mysociety.org/
+--
+
+-- Name: dayofmonthvotes; Type: TABLE
+--
+
+CREATE TABLE dayofmonthvotes (
+    id serial NOT NULL,
+    value text NOT NULL
+);
+
+--
+-- Name: dayofweekvotes; Type: TABLE
+--
+
+CREATE TABLE dayofweekvotes (
+    value text NOT NULL,
+    id serial NOT NULL
+);
+
+
+--
+-- Name: pk_dayofweekvote; Type: CONSTRAINT
+--
+
+ALTER TABLE ONLY dayofweekvotes
+    ADD CONSTRAINT pk_dayofweekvote PRIMARY KEY (id);
+
+
+--
+-- Name: pk_dayofmonthvote; Type: CONSTRAINT
+--
+
+ALTER TABLE ONLY dayofmonthvotes
+    ADD CONSTRAINT pk_dayofmonthvote PRIMARY KEY (id);
