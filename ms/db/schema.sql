46b8fabf75f2780a871ff460a5aedd92c490049c
louise <louise>
2006-02-23 12:57:14 +0000

    New table for meetup votes

--- ms/db/schema.sql
+++ ms/db/schema.sql
@@ -2,41 +2,26 @@
 -- PostgreSQL database dump
 -- schema.sql:
 -- Schema for mysociety site - first tables 
--- are formeetup day preference app (daycount)
+-- are for meetup day preference app (daycount)
 --
 -- Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 -- Email: louise.crow@gmail.com; WWW: http://www.mysociety.org/
 --
 
--- Name: dayofmonthvotes; Type: TABLE
+-- Name: meetupvotes; Type: TABLE
 --
 
-CREATE TABLE dayofmonthvotes (
+CREATE TABLE meetupvotes (
     id serial NOT NULL,
-    value text NOT NULL
-);
-
---
--- Name: dayofweekvotes; Type: TABLE
---
-
-CREATE TABLE dayofweekvotes (
-    value text NOT NULL,
-    id serial NOT NULL
+    dayofweek text NOT NULL,
+    weekofmonth text NOT NULL,
+    ipaddr text NOT NULL
 );
 
 
 --
--- Name: pk_dayofweekvote; Type: CONSTRAINT
+-- Name: pk_meetupvotes; Type: CONSTRAINT
 --
 
-ALTER TABLE ONLY dayofweekvotes
-    ADD CONSTRAINT pk_dayofweekvote PRIMARY KEY (id);
-
-
---
--- Name: pk_dayofmonthvote; Type: CONSTRAINT
---
-
-ALTER TABLE ONLY dayofmonthvotes
-    ADD CONSTRAINT pk_dayofmonthvote PRIMARY KEY (id);
+ALTER TABLE ONLY meetupvotes
+    ADD CONSTRAINT pk_meetupvotes PRIMARY KEY (id);
