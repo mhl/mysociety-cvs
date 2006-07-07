0eb63ceb75c211359d7bd811b97e033107349afa
francis <francis>
2006-07-07 11:12:35 +0000

    Add TWFY, reorder reverse chronological by launch date.

--- ms/web/projects.php
+++ ms/web/projects.php
@@ -6,16 +6,28 @@ include "wordpress/wp-content/themes/mysociety/header.php";
 <div class="item_head">
 Launched Sites
 </div>
+
 <div class="item_inner_head">
-<a href="http://www.writetothem.com">WriteToThem.com - Launched 14th February 2005</a>
+<a href="http://www.hearfromyourmp.com">HearFromYourMP.com - Launched 21st November 2005</a>
 </div>
 <div class="item">
-WriteToThem.com is the definitive place to contact any of your elected
-representatives. Enter a single postcode and it'll tell you who all
-your local representatives are, and a bit about who you should contact
-for which reasons. An award winner within 12 weeks of launching,
-WriteToThem.com sent 5000 messages in its first month of operation.
+HearFromYourMP encourages and enables MPs to run email lists for their
+constituents, and to allow those constituents to discuss ideas in a way which
+doesn't bombard them with email.  5000 people had signed up before it was even
+launched.
 </div>
+
+<div class="item_inner_head">
+<a href="http://www.pledgebank.com">PledgeBank.com - Launched 13th June 2005</a>
+</div>
+<div class="item">
+PledgeBank is about reassuring people who want to do something altruistic or
+socially beneficially that they won't be alone in their actions. It lets users
+create pledges which say "I'll do something, but only if 10 other people will
+do something", for example "I'll clean up the banks of my local river, but only
+if 5 other local people will pledge to come and help.
+</div>
+
 <div class="item_inner_head">
 <a href="http://www.notapathetic.com">NotApathetic.com - Launched 7th April 2005</a>
 </div>
@@ -27,25 +39,28 @@ attention of 40,000 visitors during the general election, and was
 discussed on a dozen local radio stations, and in newspapers from New
 Zealand to South Africa. 
 </div>
+
 <div class="item_inner_head">
-<a href="http://www.pledgebank.com">PledgeBank.com - Launched 13th June 2005</a>
+<a href="http://www.writetothem.com">WriteToThem.com - Launched 14th February 2005</a>
 </div>
 <div class="item">
-PledgeBank is about reassuring people who want to do something altruistic or
-socially beneficially that they won't be alone in their actions. It lets users
-create pledges which say "I'll do something, but only if 10 other people will
-do something", for example "I'll clean up the banks of my local river, but only
-if 5 other local people will pledge to come and help.
+WriteToThem.com is the definitive place to contact any of your elected
+representatives. Enter a single postcode and it'll tell you who all
+your local representatives are, and a bit about who you should contact
+for which reasons. An award winner within 12 weeks of launching,
+WriteToThem.com sent 5,000 messages in its first month of operation.
 </div>
+
 <div class="item_inner_head">
-<a href="http://www.hearfromyourmp.com">HearFromYourMP.com - Launched 21st November 2005</a>
+<a href="http://www.theyworkforyou.com">TheyWorkForYou.com - Launched 6th June 2004</a>
 </div>
 <div class="item">
-HearFromYourMP encourages and enables MPs to run email lists for their
-constituents, and to allow those constituents to discuss ideas in a way which
-doesn't bombard them with email.  5000 people had signed up before it was even
-launched.
+TheyWorkForYou provides a searchable, annotatable version of what is said in
+Parliament, as well as useful pages providing clear, non-biased information on
+a range of different measures of activities by MPs.  Originally built by
+volunteers while mySociety was getting started, it is now part of mySociety.
 </div>
+
 <div class="item">
 <strong>We appreciate feedback on all our live sites, so please tell us what we can improve.</strong>
 </div>
