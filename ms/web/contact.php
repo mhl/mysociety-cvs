243b47423268b07d19d898546e19f32db6e4e850
francis <francis>
2006-08-24 11:53:00 +0000

    And contact page

---
+++ ms/web/contact.php
@@ -0,0 +1,38 @@
+<?php 
+include "wordpress/wp-blog-header.php";
+include "wordpress/wp-content/themes/mysociety/header.php"; 
+?>
+
+<div class="item_head">
+mySociety.org - Contact us
+</div>
+<div class="item">
+  <p><b>By email<br/>
+  </b> Email the mySociety team <a href="mailto:team@mysociety.org">team@mysociety.org</a>,
+  or just Tom Steinberg <a href="mailto:tom@mysociety.org">tom@mysociety.org</a>, 
+
+  <p><b>By phone<br/>
+  </b> Call Tom Steinberg on <b>07811 082158</b>.</p>
+
+  <p><b>Chat room<br/>
+  </b> Join our <a href="http://www.irc.mysociety.org">public Internet chat room</a>.</p>
+
+  <p><b>By post<br/>
+  </b> 
+   <blockquote>mySociety.org
+  <br>12 Duke's Road
+  <br>London
+  <br>WC1H 9AD</blockquote>
+  (We don't actually work here, as we're one of those new fangled organisations
+  where everyone works from home spread across the world. It's where our charity
+  is registered)
+ </p>
+
+</div>
+
+
+
+<div class="item_foot">
+</div>
+<?php include "wordpress/wp-content/themes/mysociety/footer.php"; ?>
+
