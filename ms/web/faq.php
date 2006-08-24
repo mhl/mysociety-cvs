b84223ea26238cee3d6d45994f8e017370f4ff78
francis <francis>
2006-08-24 11:09:54 +0000

    Refresh of the FAQ, it was shockingly out of date. Still needs a proper rewrite, thinking more carefully about how we present ourselves.

--- ms/web/faq.php
+++ ms/web/faq.php
@@ -18,23 +18,42 @@ include "wordpress/wp-content/themes/mysociety/header.php";
   A.</b> Email <a href="mailto:tom@mysociety.org">Tom Steinberg</a> or call 07811 082158.</p>
   <a name="do" id="do"></a>
   <p><b>Q. What exactly are you doing?<br/>
-  A.</b> We are supporting and improving our <a href="projects.php">launch projects</a>. 
-  And lots of other things as <a href="http://www.mysociety.org/2006/07/21/summer-daze/">described
-  in this post</a> on our developers' blog.</p>
+  A.</b> We are supporting and improving our <a href="projects.php">launch projects</a>.</p>
   <p><b>Q. Can you give us any examples of the types of project you're on about?<br/>
   A.</b> <a href="http://www.theyworkforyou.com">TheyWorkForYou.com</a>, <a
   href="http://writetothem.com">WriteToThem</a> (which used to be called FaxYourMP) and <a
   href="http://www.pledgebank.com">PledgeBank</a> are all examples of the type
   of service which we aim to foster. But it is exactly the rarity of such
-  really useful, effective, cheap civic sites that has lead to mySociety's
+  really useful, effective, cheap civic sites that led to mySociety's
   creation</p>
-<p><b>Q. What have you been doing for the last two years?<br/>
-  A.</b> mySociety was founded in September 2003, but it only started building projects in September 2004. The reason for this is, strangely, that we got very lucky. The ODPM told us in March 2004 that we were being awarded a substantial sum of money in partnership with West Sussex County Council, which was the single most important event in mySociety's brief history. However, government handling of funding has to be extremely cautious, and it took until September 2004 for us to actually recieve money to start development. Since then, we've been racing along!</p>
+<p><b>Q. What have you been doing for the last three years?<br/>
+  A.</b> mySociety was founded in September 2003.  We spent the first year
+  raising money, getting a substantial grant from a Government department
+  called ODPM, in partnership with West Sussex County Council. The money
+  actually arrived in September 2004. We raced along for a year, building
+  all of our <a href="projects.php">launch projects</a>. Since then we've
+  been improving our existing projects, and starting new ones, some <a
+  href="http://www.mysociety.org/2006/07/21/summer-daze/">described in this
+  post</a> on our developers' blog. We're getting money from 
+  <a href="http://www.mysociety.org/2006/06/05/this-is-what-transparency-means/">various
+  foundations and bits of government</a>.
+  </p>
 
-<p><b>Q. So what is this funding about?<br/>
-A.</b> In March 2004 we were awarded £250,000 in a joint bid with West Sussex County Council. The money came from the Office of the Deputy Prime Minister, in particular from their <a href="http://www.kablenet.com/kd.nsf/Frontpage/8FD7C45C4B155DAC80256DA40043FE4C?OpenDocument">e-innovations fund</a>. We are immensely grateful, and hopeful that we will show how such sums, often small by e-government standards, can go a long way when used wisely.</p>
-
-		<p><b>Q. Who are you lot?<br/>A.</b> Tom Steinberg is the project's founder and its only full timer. By trade he was a Westminster policy wonk, serving time with the Prime Minister's Strategy Unit. In his spare time he is a technology aficionado, having been a SysAdmin, a gamer, and having written opinion pieces for the Register. Lots of other people have helped get mySociety.org this far, including an <a href="http://www.mysociety.org/cgi-bin/moin.cgi/AdvisoryPanel">advisory panel</a>, as well as lots of fine people from other organisations including several of the <a href="http://www.faxyourmp.com">FaxYourMp.com</a> mob.  <a href="http://www.unfortu.net/~james/">James Cronin</a> has been running around setting things up behind the scenes, <a href="http://www.tomski.com/">Tomski</a> and <a href="http://www.j-dom.org">Jason Kitcat</a> hacked together this website in a real hurry, and then <a href="http://www.mysociety.org/cgi-bin/moin.cgi/FrancisIrving">Francis Irving</a> rebuilt it when we got some time. The core team of paid developers consists of Francis, plus <a href="http://www.mysociety.org/cgi-bin/moin.cgi/ChrisLightfoot">Chris Lightfoot</a> and <a href="http://www.mysociety.org/cgi-bin/moin.cgi/MatthewSomerville">Matthew Somerville</a>.</p>
+        <p><b>Q. Who are you lot?<br/>A.</b> <a href="http://www.mysociety.org/moin.cgi/TomSteinberg">Tom Steinberg</a>
+        is the project's founder, a Westminster policy wonk and technology aficionado. He's
+        joined by three full time developers, 
+        <a href="http://www.mysociety.org/cgi-bin/moin.cgi/FrancisIrving">Francis Irving</a>,
+        <a href="http://www.mysociety.org/cgi-bin/moin.cgi/ChrisLightfoot">Chris Lightfoot</a> and
+        <a href="http://www.mysociety.org/cgi-bin/moin.cgi/MatthewSomerville">Matthew Somerville</a>,
+        who've all started projects making mySociety style websites in the past.
+        A whole bunch of super fine volunteers build and improve mySociety
+        websites for fun.  They're credited on the individual websites. You can
+        <a href="/volunteertasks">be one too</a>.  Lots of other volunteers
+        help run mySociety in other ways. For example, by being on our <a
+        href="http://www.mysociety.org/cgi-bin/moin.cgi/AdvisoryPanel">advisory
+        panel</a>. You can never thank someone enough for doing VAT
+        registration for you, you know who you are <a href="http://www.unfortu.net/~james/">James</a>.
+</p>
 
         <p><b>Q. Are you a registered charity?<br/>A.</b> mySociety is the
 project of a registered charity. The charity is called UK Citizens Online Democracy and is <a href="http://www.charity-commission.gov.uk/registeredcharities/showcharity.asp?remchar=&chyno=1076346">charity number 1076346</a>. UKCOD doesn't do much else these days, so don't worry if you get confused between the two. We do as well!
@@ -42,29 +61,68 @@ project of a registered charity. The charity is called UK Citizens Online Democr
 		<p><b>Q. Where did the idea come from?<br/>A.</b> mySociety represents the crystallisation of a lot of widely shared thoughts and concerns about the problems facing democracy, government and technology in the UK at the moment. James Crabtree first gave the idea formal shape in an <a href="http://www.opendemocracy.net/debates/article-8-85-1025.jsp">OpenDemocracy article</a>. Then Tom took the idea, gave it a polish, and set up mySociety.</p>
 	        
 		<p><b>Q. So you're an incubator?<br/>A.</b> Not really, although we'd love to work with more partners. mySociety is keen to build sites which embody certain core principles, such as cheap scalability, really tangible outputs, and high usability. We're not a web agency, and won't build anything we don't believe is worthwhile, but if you have an idea and you think we might be able to help, please get in touch.</p>
-		<p><b>Q. Will the projects be open source?<br/>A.</b> We are planning that all projects will be open source, unless some really compelling situation arises forcing a project not to be. You can find our current CVS repository <a href="https://secure.mysociety.org/cvstrac/dir?d=mysociety">here</a>.</p>
+        <p><b>Q. Are the projects open source?<br/>A.</b> Nearly all of our
+        code is open source, and will be in the future, unless some 
+        compelling situation arises forcing a project not to be. You can find
+        our CVS repository <a
+        href="https://secure.mysociety.org/cvstrac/dir?d=mysociety">here</a>. 
+        We've got a private repository as well, with server configuration stuff in it.
+        </p>
 
-		<p><b>Q. Why doesn't the private sector run these projects for a profit?<br/>A.</b> If they did, we wouldn't have to invent them! Seriously, though, we're not going to reproduce what is done effectively in the private sector. Furthermore, if companies decide after a project has been created that they would like to adopt it and foster it in exchange for further development capital, we'll be happy to talk to them about it.</p>
+        <p><b>Q. Why doesn't the private sector run these projects for a
+        profit?<br/>A.</b> If they did, we wouldn't have to invent them!
+        Seriously, though, we're not going to reproduce what is done
+        effectively in the private sector. Furthermore, if companies decide
+        after a project has been created that they would like to adopt it and
+        foster it in exchange for further development capital, we'll be happy
+        to talk to them about it.</p>
 		
 		<p><b>Q. Why don't you leave projects like this up to the Open source community?<br/>A.</b> The Open source and free software communities have a formidable track record of creating powerful and useful tools without which the internet would probably not exist. Most, if not all of the projects developed here will have their Source code released under open source licences, and we will be hoping to build voluntary development communities around all our projects. However, we feel that the track-record of the voluntary development community when it comes to developing socially focussed tools with off line impacts is perhaps less than it might be. There are all sorts of reasons for this, but the upshot is that we feel that a supportive structure and perhaps some paid key developers are crucial to innovation in a sector which is a terra incognita for many in the open source world. After all, even Linus Torvalds had the security of his academic studies to pay the bills...</p>
 
-		<p><b>Q. Will users be expected to pay for services?<br/>A.</b> No. There would have to be a very unusual and compelling basis for charging to become part of a project's structure. Sometimes partners who want to syndicate our sites will be asked to contribute.</p>
+        <p><b>Q. Are users be expected to pay for services?<br/>A.</b> No.
+        There would have to be a very unusual and compelling basis for charging
+        to become part of a project's structure. Sometimes partners who want to
+        syndicate our sites will be asked to contribute.</p>
+
 		<a name="why" id="why"></a>
 		
 		<a name="pay" id="pay"></a>
 		<p><b>Q. What is this about people getting paid?<br/>
-		A.</b> We believe that in order to deliver polished, highly usable social tools, developers need at least a short period of financially stable full time employment. We pay rates which are living wages, but which well below the market rate, especially for people of the talent we employ. The gratitude of the director (Tom Steinberg) and all the project's Trustees goes to the developers for working on these terms.
+        A.</b> We believe that in order to deliver polished, highly usable
+        social tools, developers need at least a short period of financially
+        stable full time employment. We pay rates which are living wages, but
+        which well below the market rate, especially for people of the talent
+        we employ. The gratitude of the director (Tom Steinberg) and all the
+        project's Trustees goes to the developers for working on these terms.
 
-		<p><b>Q. I'm not a skilled techy, but I've got an idea. Why should I tell you?<br/>A.</b> First, we offer the possibility that your idea will be made into a real working project delivering useful services. Also, we offer the chance to be engaged in the process of realisation, even if you don't know your Perl from your C++. Also, numbers allowing, you'll get an invite to one of our occasional pub-based evenings to discuss projects. Lastly, we offer the prospect of Tim Berners-Lee style eternal fame. Well, the prospect anyway.</p>
-
-		<p><b> Q. I've not got a particular idea in mind, but I think it all sounds very interesting. Can I help?<br/>A.</b> Yes! We are looking for help of all kinds at the moment. Anything from marketing, to fund raising, to graphic design, human computer interface work, anything. If you're interested, please <a href="mailto:tom@mysociety.org?subject=Help">email</a> us. And yes, you'll hopefully get an invite to our London brainstorm bash, too.</p>
+        <p><b> Q. Can I help?<br/>A.</b> Yes! We are looking for
+        help of all kinds at the moment. Anything from marketing, to fund
+        raising, to graphic design, to programming, human computer interface
+        work, anything. If you're interested, please <a
+        href="mailto:tom@mysociety.org?subject=Help">email</a> us or check
+        out our <a href="/volunteertasks">volunteer tasks</a> page.  </p>
 
 				<p><b>Q. I'm not in Britain - does any of this matter to
-        me?<br/>A.</b> Yes! Whilst our home country is the UK, we do not see ourselves as limited to it in any way. We will gladly speak to and work with people from outside the UK, and it is our hope and expectation that people around the world will be able to use and adopt the open source tools and services we develop for use in their own countries. We will even consider developing projects based in other countries, if appropriate funding can be found.</p>
+        me?<br/>A.</b> Yes! Whilst our home country is the UK, we do not see
+        ourselves as limited to it in any way.  We gladly speak to and work
+        with people from outside the UK.  People around the world can use and
+        adopt our open source tools and services for use in their own
+        countries. <a href="http://www.pledgebank.com">PledgeBank</a>, for
+        example, is a truly international project.  We will consider developing
+        projects based in other countries, if appropriate funding can be
+        found.</p>
 
 		<p><b>Q. Do you have a political agenda?<br/>A.</b> No, we are not party political, and this project is neither left or right-wing. It is about building useful digital tools for anyone who wants to use them.</p>
+
+        <p><b>Q. Who built this website, www.mysociety.org?<br/>A.</b> 
+        <a href="http://www.tomski.com/">Tomski</a> and <a
+        href="http://www.j-dom.org">Jason Kitcat</a> hacked together the first
+        version in a real hurry. We've been rebuilding it ever since. 
+
 		<p><b>Q. How can I be kept up to date with the project?<br/>A.</b> Please join our <a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/news">mailing list</a>. All normal no-moronic-spamming policies apply.</p>
+
         </div>
+
 
 <div class="item_foot">
 </div>
