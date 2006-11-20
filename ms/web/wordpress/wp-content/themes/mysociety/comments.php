9a672281e91f29c8abdcbedfbb962090f49f4ff3
francis <francis>
2006-11-20 06:53:44 +0000

    Change magic word back to 'tangible' now have discovered it was trackbacks on old posts causing the spam.

--- ms/web/wordpress/wp-content/themes/mysociety/comments.php
+++ ms/web/wordpress/wp-content/themes/mysociety/comments.php
@@ -104,7 +104,7 @@
 
 <p><textarea name="comment" id="comment" style="width:100%" cols="60" rows="10" tabindex="4"></textarea></p>
 
-<p>Enter the magic word, which is 'simple':
+<p>Enter the magic word, which is 'tangible':
 <input name="magicword" type="input" id="magicword" tabindex="5"
 value="" />
 
