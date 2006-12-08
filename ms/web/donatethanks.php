458902860f3c6adbafea7d424d42546749e28a9a
ben <ben>
2006-12-08 18:48:47 +0000

    Added some text to try and help give donors a warm fuzzy glow.

--- ms/web/donatethanks.php
+++ ms/web/donatethanks.php
@@ -17,6 +17,22 @@ include "wordpress/wp-content/themes/mysociety/header.php";
 has been emailed to you. If you have a paypal account, you may log
 into your account at <a href="http://www.paypal.com/">www.paypal.com</a>
 to view details of this transaction.</p>
+
+<p>
+Numerous independent studies have shown conclusively that donations
+to mySociety are effective in:
+<ul>
+<li>helping to combat climate change</li>
+<li>increasing the happiness of whales, baby seals and kittens</li>
+<li>preventing baldness</li>
+</ul>
+So please... take a little time to pat yourself on the back. Maybe you
+could have a nice cup of tea and a sit down.
+</p>
+<p>
+You've earned it.
+</p>
+
 </div>
 <div class="item_foot">
 </div>
