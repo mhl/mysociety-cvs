a010450f8a19a6165c0249b32338ed3f58f374b6
ben <ben>
2006-12-08 16:44:08 +0000

    Donation Thankyou/Cancel pages for paypal auto_return

---
+++ ms/web/donatecancel.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * User is redirected to this page by paypal auto_return after
+ * cancelled donation - boohoo!!!
+ */
+
+include "wordpress/wp-blog-header.php";
+include "wordpress/wp-content/themes/mysociety/header.php"; 
+?>
+
+<div class="item_head">
+  Donation cancelled!
+</div>
+
+<div class="item">
+<p>
+We're sorry to hear it, but your donation was cancelled.
+</p>
+</div>
+<div class="item_foot">
+</div>
+
+<?php include "wordpress/wp-content/themes/mysociety/footer.php"; ?>
+
---
+++ ms/web/donatethanks.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+ * User is redirected to this page by paypal auto_return after
+ * successful donation
+ */
+
+include "wordpress/wp-blog-header.php";
+include "wordpress/wp-content/themes/mysociety/header.php";
+?>
+
+<div class="item_head">
+  Thank you for your donation!
+</div>
+
+<div class="item">
+<p>
+Your transaction has been completed, and a receipt for your purchase has been emailed to you. If you have a paypal account, you may log into your account at www.paypal.com to view details of this transaction.
+</p>
+</div>
+<div class="item_foot">
+</div>
+
+<?php include "wordpress/wp-content/themes/mysociety/footer.php"; ?>
+
