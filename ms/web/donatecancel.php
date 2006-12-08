e26677160552eea3d391f6727d5114cbe06f8550
ben <ben>
2006-12-08 19:16:52 +0000

    Pathos! Drama! We've got it all!

--- ms/web/donatecancel.php
+++ ms/web/donatecancel.php
@@ -16,6 +16,26 @@ include "wordpress/wp-content/themes/mysociety/header.php";
 <p>
 We're sorry to hear it, but your donation was cancelled.
 </p>
+
+<p>
+<small>No no, it's OK...</small>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+* Sniff *<br/>
+<br/>
+<small>...just something in our eye.
+<br/>
+<br/>
+Really.
+<br/>
+<br/>
+We'll be fine.</small>
+</p>
+
 </div>
 <div class="item_foot">
 </div>
