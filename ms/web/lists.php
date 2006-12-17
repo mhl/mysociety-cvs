9d93d4a8760df48f751767a03bbab93d5a26d06a
adam <adam>
2006-12-17 16:36:31 +0000

    list of lists with archive links, to replace link to mailman/listinfo

---
+++ ms/web/lists.php
@@ -0,0 +1,64 @@
+<?php 
+include "wordpress/wp-blog-header.php";
+include "wordpress/wp-content/themes/mysociety/header.php"; 
+?>
+
+<div class="item_head">
+Email Lists</div>
+
+<div class="item_inner_head">
+<a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/cvs-public">cvs-public</a>
+</div>
+<div class="item">
+CVS commits for mySociety's public CVS repository<br /><strong>This list is not archived</strong>
+</div>
+
+<div class="item_inner_head">
+<a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/developers-public">developers-public</a>
+</div>
+<div class="item">
+mySociety projects discussion for developers, volunteers and anyone else interested - join this one if stuck!<br /><a href="https://secure.mysociety.org/admin/lists/pipermail/developers-public/">List Archive</a>
+</div>
+
+<div class="item_inner_head">
+<a href="https://secure.mysociety.org/admin/lists/pipermail/internationalisation/">internationalisation</a>
+</div>
+<div class="item">
+Discussion of internationalisation of mySociety projects<br /><a href="https://secure.mysociety.org/admin/lists/pipermail/internationalisation/">List Archive</a>
+</div>
+
+<div class="item_inner_head">
+<a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/maps">maps</a>
+</div>
+<div class="item">
+mySociety's maps and geography discussion list<br /><a href="https://secure.mysociety.org/admin/lists/pipermail/maps/">List Archive</a>
+</div>
+
+<div class="item_inner_head">
+<a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/news">news</a>
+</div>
+<div class="item">
+Important news releases from mySociety<br /><a href="https://secure.mysociety.org/admin/lists/pipermail/news/">List Archive</a>
+</div>
+<div class="item_foot">
+</div>
+
+<div class="item_inner_head">
+<a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/nongeek-volunteers">nongeek-volunteers</a>
+</div>
+<div class="item">
+Non-geek mySociety volunteers<br /><a href="https://secure.mysociety.org/admin/lists/pipermail/nongeek-volunteers/">List Archive</a>
+</div>
+<div class="item_foot">
+</div>
+
+<div class="item_inner_head">
+<a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/theyworkforyou-scotland">theyworkforyou-scotland</a>
+</div>
+<div class="item">
+A list to discuss creating a Scottish Parliament version of TheyWorkForYou<br /><a href="https://secure.mysociety.org/admin/lists/pipermail/theyworkforyou-scotland/">List Archive</a>
+</div>
+<div class="item_foot">
+</div>
+
+<?php include "wordpress/wp-content/themes/mysociety/footer.php"; ?>
