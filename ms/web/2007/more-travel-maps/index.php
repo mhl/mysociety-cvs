827bb323802cf5c773008cd4c40ca80ee971d119
mswww-staging <mswww-staging>
2007-01-27 13:13:18 +0000

    Shell for second DfT report

---
+++ ms/web/2007/more-travel-maps/index.php
@@ -0,0 +1,98 @@
+<?php 
+/* XXX can't override the title easily, which will make us look like total
+ * morons. For this page it should be, "Travel-time maps". */
+include "../../wordpress/wp-blog-header.php";
+include "../../wordpress/wp-content/themes/mysociety/header.php"; 
+?>
+
+<h1>More travel-time Maps and their Uses</h1>
+
+<p>Chris Lightfoot, mySociety &lt;<a href="mailto:chris@mysociety.org">chris@mysociety.org</a>&gt;<br>
+Francis Irving, mySociety &lt;<a href="mailto:francis@mysociety.org">francis@mysociety.org</a>&gt;<br>
+Tom Steinberg, mySociety &lt;<a href="mailto:tom@mysociety.org">tom@mysociety.org</a>&gt;<br>
+</p>
+
+<p>This work was funded and supported by the <a href="http://www.dft.gov.uk/">Department&nbsp;for&nbsp;Transport</a>.</p>
+
+<ul>
+<li><a href="#introduction">Introduction</a></li>
+<li><a href="#legibilirty">Improving legibility</a></li>
+<li><a href="#houseprices">House prices</a></li>
+<li><a href="#cartrain">Car vs. public transport</a></li>
+<li><a href="#realtime">Real time maps</a></li>
+<li><a href="#furtherwork">What next? Further work</a></li>
+<li><a href="#acknowledgments">Acknowledgments</a></li>
+<li><a href="#data">The data we used</a></li>
+</ul>
+
+<!--<p>(See also: <a href="methods.php">description of methods</a>;
+<a href="slides.pdf">presentation slides</a>. You may also be interested in
+<a href="https://secure.mysociety.org/admin/lists/mailman/listinfo/maps">the
+mySociety maps mailing list</a>.)</p> -->
+
+<h2><a name="introduction"></a>Introduction</h2>
+
+<p>This is a follow on report from the one we made last year called
+<a href="/2006/travel-time-maps/">Travel-time Maps and their Uses</a>
+
+<h2><a name="legibility"></a>Improving legibility</h2>
+
+<p>Discrete vs. continuous contours. Better mapping backgrounds.
+
+<p>Travelling to work by 9am
+
+<h2><a name="houseprices"></a>House prices</h2>
+
+<p>London house price data
+
+<p><a href="rail-cambridge-1500px.png"><img src="rail-cambridge-500px.png" width="288" height="500" alt="Map showing travel times by rail and taxi from Cambridge to other points in Great Britain, starting at 7 o'clock on a weekday morning"></a>
+<br><small>&copy; Crown copyright. All rights reserved. Department&nbsp;for&nbsp;Transport&nbsp;100020237&nbsp;2006</small>
+</p>
+
+<h2><a name="cartrain"></a>Car vs. public transport</h2>
+
+<h2><a name="realtime"></a>Real time maps</h2>
+
+<p>When they see these
+
+<p>Please contact us (email addresses at the top of the page) if you have
+any questions or comments. If you're interested in this area, please also
+sign up to the (fairly low-traffic)
+<a href="http://secure.mysociety.org/admin/lists/mailman/listinfo/maps">mySociety maps
+mailing list</a>.</p>
+
+<h2><a name="realtime"></a>What next? Further work</h2>
+
+<p>Two places of work (for couples)
+<p>Where should I look for work that I can get to easily?
+<p>Travel time to nearest hospital
+
+<h2><a name="acknowledgments"></a>Acknowledgments</h2>
+
+<p>This work was funded by the
+<a href="http://www.dft.gov.uk/">Department&nbsp;for&nbsp;Transport</a>, who
+also made it possible for us to use
+<a href="http://www.ordsvy.co.uk/">Ordnance Survey</a> maps and data through
+their licence; without this assistance we would have had to pay expensive fees
+to use the underlying mapping data or to produce maps with no landmarks, which
+would be almost incomprehensible. DfT also gave us access to their
+<a href="http://www.naptan.org.uk/">National Public Transport Access Node</a>
+database, which records the locations of train and tube stations and bus stops; without this it would have been difficult to produce any maps at all.</p>
+
+<h2><a name="data"></a>The data we used</h2>
+
+<p>Although the journey planning services and software we used were publicly
+accessibly, almost none of the other data is available unless you pay for it,
+or your work falls under an existing licencing agreement. So while we set out
+to demonstrate how easily we could make travel-time maps from public data, very
+little of this work could be cheaply reproduced or extended without assistance
+from a government department.</p>
+
+<p>That's unfortunate, because it means that innovative work by outsiders in
+this area can only go ahead if it's explicitly sponsored by government. If all
+the data we've used had been available for free, somebody else might well have
+done what we've done years ago, with no cost to the taxpayer. We'd love it if
+others extend the work that we've done, but realistically there aren't very
+many people in a position to do this cheaply.</p>
+
+<?php include "wordpress/wp-content/themes/mysociety/footer.php"; ?>
