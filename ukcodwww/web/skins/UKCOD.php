8b5844154d098f7327ef5a2d6e51063da0781b63
francis <francis>
2007-04-19 14:50:09 +0000

    Slightly more sensible theming

--- ukcodwww/web/skins/UKCOD.php
+++ ukcodwww/web/skins/UKCOD.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * See skin.txt
+ * UKCOD theme
  *
  * @todo document
  * @package MediaWiki
@@ -8,16 +8,19 @@
  */
 
 if( !defined( 'MEDIAWIKI' ) )
-	die();
+	die( -1 );
 
+/** */
 require_once('includes/SkinTemplate.php');
 
 /**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
  * @todo document
  * @package MediaWiki
  * @subpackage Skins
  */
 class SkinUKCOD extends SkinTemplate {
+	/** Using ukcod. */
 	function initPage( &$out ) {
 		SkinTemplate::initPage( $out );
 		$this->skinname  = 'ukcod';
@@ -33,7 +36,7 @@ class SkinUKCOD extends SkinTemplate {
  */
 class UKCODTemplate extends QuickTemplate {
 	/**
-	 * Template filter callback for MonoBook skin.
+	 * Template filter callback for UKCOD skin.
 	 * Takes an associative array of data set from a SkinTemplate-based
 	 * class, and a wrapper for MediaWiki's localization database, and
 	 * outputs a formatted page.
@@ -43,160 +46,195 @@ class UKCODTemplate extends QuickTemplate {
 	function execute() {
 		// Suppress warnings to prevent notices about missing indexes in $this->data
 		wfSuppressWarnings();
-        
-        include($_SERVER['DOCUMENT_ROOT']."/header.html");
-?>
 
-    <div id="globalWrapper">
-      <div id="column-content">
+        include($_SERVER['DOCUMENT_ROOT']."/header.html");
+
+?>	<div id="globalWrapper">
+		<div id="column-content">
 	<div id="content">
-	  <a name="top" id="top"></a>
-<div class="editsection" style="float:right;margin-left:5px;">[<a href="<?php echo htmlspecialchars($this->data['content_actions']['edit']['href']) ?>"">edit page</a>]</div>
-      <?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
-	  <h1 class="firstHeading"><?php $this->text('title') ?></h1>
-	  <div id="bodyContent">
-	    <!--<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>-->
-	    <div id="contentSub"><?php $this->html('subtitle') ?></div>
-	    <?php if($this->data['undelete']) { ?><div id="contentSub"><?php     $this->html('undelete') ?></div><?php } ?>
-	    <?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
-    <?php /* ?>
+		<a name="top" id="top"></a>
+		<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+		<h1 class="firstHeading"><?php $this->data['displaytitle']!=""?$this->html('title'):$this->text('title') ?></h1>
+		<div id="bodyContent">
+			<div id="contentSub"><?php $this->html('subtitle') ?></div>
+			<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php     $this->html('undelete') ?></div><?php } ?>
+			<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
+			<?php /*if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } */?>
+			<!-- start content -->
+			<?php $this->html('bodytext') ?>
+			<?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
+			<!-- end content -->
+			<div class="visualClear"></div>
+		</div>
+	</div>
+		</div>
+		<div id="column-one">
 	<div id="p-cactions" class="portlet">
-	  <h5><?php $this->msg('views') ?></h5>
-	  <ul>
-	    <?php foreach($this->data['content_actions'] as $key => $action) {
-	       ?><li id="ca-<?php echo htmlspecialchars($key) ?>"
-	       <?php if($action['class']) { ?>class="<?php echo htmlspecialchars($action['class']) ?>"<?php } ?>
-	       ><a href="<?php echo htmlspecialchars($action['href']) ?>"><?php
-	       echo htmlspecialchars($action['text']) ?></a></li><?php
-	     } ?>
-	  </ul>
+		<h5><?php $this->msg('views') ?></h5>
+		<div class="pBody">
+			<ul>
+	<?php			foreach($this->data['content_actions'] as $key => $tab) { ?>
+					 <li id="ca-<?php echo Sanitizer::escapeId($key) ?>"<?php
+					 	if($tab['class']) { ?> class="<?php echo htmlspecialchars($tab['class']) ?>"<?php }
+					 ?>><a href="<?php echo htmlspecialchars($tab['href']) ?>"><?php
+					 echo htmlspecialchars($tab['text']) ?></a></li>
+	<?php			 } ?>
+			</ul>
+		</div>
 	</div>
-    <?php */ ?>
-	    <!-- start content -->
-	    <?php $this->html('bodytext') ?>
-	    <?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
-	    <!-- end content -->
-	    <div class="visualClear"></div>
-	  </div>
-	</div>
-      </div>
-      <hr>
-      <div id="column-one">
 	<div class="portlet" id="p-personal">
-	  <h5><?php $this->msg('personaltools') ?></h5>
-	  <div class="pBody">
-	    <ul>
-	    <?php foreach($this->data['personal_urls'] as $key => $item) {
-	       ?><li id="pt-<?php echo htmlspecialchars($key) ?>"><a href="<?php
-	       echo htmlspecialchars($item['href']) ?>"<?php
-	       if(!empty($item['class'])) { ?> class="<?php
-	       echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
-	       echo htmlspecialchars($item['text']) ?></a></li><?php
-	    } ?>
-	    </ul>
-	  </div>
+		<h5><?php $this->msg('personaltools') ?></h5>
+		<div class="pBody">
+			<ul>
+<?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
+				<li id="pt-<?php echo Sanitizer::escapeId($key) ?>"<?php
+					if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
+				echo htmlspecialchars($item['href']) ?>"<?php
+				if(!empty($item['class'])) { ?> class="<?php
+				echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
+				echo htmlspecialchars($item['text']) ?></a></li>
+<?php			} ?>
+			</ul>
+		</div>
 	</div>
-    <?php /* ?>
 	<script type="<?php $this->text('jsmimetype') ?>"> if (window.isMSIE55) fixalpha(); </script>
 	<?php foreach ($this->data['sidebar'] as $bar => $cont) { ?>
-	<div class='portlet' id='p-<?php echo htmlspecialchars($bar) ?>'>
-	  <h5><?php $out = wfMsg( $bar ); if (wfNoMsg($bar, $out)) echo $bar; else echo $out; ?></h5>
-	  <div class='pBody'>
-	    <ul>
-	    <?php foreach($cont as $key => $val) { ?>
-	      <li id="<?php echo htmlspecialchars($val['id']) ?>"><a href="<?php echo htmlspecialchars($val['href']) ?>"><?php echo htmlspecialchars($val['text'])?></a></li>
-	     <?php } ?>
-	    </ul>
-	  </div>
+	<div class='portlet' id='p-<?php echo Sanitizer::escapeId($bar) ?>'>
+		<h5><?php $out = wfMsg( $bar ); if (wfEmptyMsg($bar, $out)) echo $bar; else echo $out; ?></h5>
+		<div class='pBody'>
+			<ul>
+<?php 			foreach($cont as $key => $val) { ?>
+				<li id="<?php echo Sanitizer::escapeId($val['id']) ?>"<?php
+					if ( $val['active'] ) { ?> class="active" <?php }
+				?>><a href="<?php echo htmlspecialchars($val['href']) ?>"><?php echo htmlspecialchars($val['text']) ?></a></li>
+<?php			} ?>
+			</ul>
+		</div>
 	</div>
 	<?php } ?>
-    <?php */ ?>
 	<div id="p-search" class="portlet">
-	  <div class="pBody">
-	    <form name="searchform" action="<?php $this->text('searchaction') ?>" id="searchform">
-          <label for="searchInput"><?php $this->msg('search') ?></label>
-	      <input id="searchInput" name="search" type="text"
-	        <?php if($this->haveMsg('accesskey-search')) {
-	          ?>accesskey="<?php $this->msg('accesskey-search') ?>"<?php }
-	        if( isset( $this->data['search'] ) ) {
-	          ?> value="<?php $this->text('search') ?>"<?php } ?> />
-	      <input type='submit' name="go" class="searchButton" id="searchGoButton"
-	        value="<?php $this->msg('go') ?>"
-	        />&nbsp;<input type='submit' name="fulltext"
-	        class="searchButton"
-	        value="<?php $this->msg('search') ?>" />
-	    </form>
-	  </div>
+		<h5><label for="searchInput"><?php $this->msg('search') ?></label></h5>
+		<div id="searchBody" class="pBody">
+			<form action="<?php $this->text('searchaction') ?>" id="searchform"><div>
+				<input id="searchInput" name="search" type="text" <?php
+					if($this->haveMsg('accesskey-search')) {
+						?>accesskey="<?php $this->msg('accesskey-search') ?>"<?php }
+					if( isset( $this->data['search'] ) ) {
+						?> value="<?php $this->text('search') ?>"<?php } ?> />
+				<input type='submit' name="go" class="searchButton" id="searchGoButton"	value="<?php $this->msg('searcharticle') ?>" />&nbsp;
+				<input type='submit' name="fulltext" class="searchButton" id="mw-searchButton" value="<?php $this->msg('searchbutton') ?>" />
+			</div></form>
+		</div>
 	</div>
+<? /* ?>
 	<div class="portlet" id="p-tb">
-	  <!--<h5>More</h5>-->
-	  <div class="pBody">
-	    <ul>
-		  <?php if($this->data['notspecialpage']) { foreach( array( 'whatlinkshere', 'recentchangeslinked' ) as $special ) { ?>
-		  <li id="t-<?php echo $special?>"><a href="<?php
-		    echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
-		    ?>"><?php echo $this->msg($special) ?></a></li>
-		  <?php } } ?>
-              <?php if(isset($this->data['nav_urls']['trackbacklink'])) { ?>
-		  <li id="t-trackbacklink"><a href="<?php
-		    echo htmlspecialchars($this->data['nav_urls']['trackbacklink']['href'])
-		    ?>"><?php echo $this->msg('trackbacklink') ?></a></li>
-	      <?php } ?>
-	      <?php if($this->data['feeds']) { ?><li id="feedlinks"><?php foreach($this->data['feeds'] as $key => $feed) {
-	        ?><span id="feed-<?php echo htmlspecialchars($key) ?>"><a href="<?php
-	        echo htmlspecialchars($feed['href']) ?>"><?php echo htmlspecialchars($feed['text'])?></a>&nbsp;</span>
-	        <?php } ?></li><?php } ?>
-	      <?php foreach( array('contributions', 'emailuser', 'upload', 'specialpages', 'recentchanges') as $special ) { ?>
-	      <?php if($this->data['nav_urls'][$special]) {?><li id="t-<?php echo $special ?>"><a href="<?php
-	        echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
-	        ?>"><?php $this->msg($special) ?></a></li><?php } ?>
-	      <?php } ?>
-	      <?php if(!empty($this->data['nav_urls']['print']['href'])) { ?>
-	      <li id="t-print"><a href="<?php
-		    echo htmlspecialchars($this->data['nav_urls']['print']['href'])
-		    ?>"><?php echo $this->msg('printableversion') ?></a></li>
-	      <?php } ?>
-	    </ul>
-	  </div>
-	</div>
-	<?php if( $this->data['language_urls'] ) { ?><div id="p-lang" class="portlet">
-	  <h5><?php $this->msg('otherlanguages') ?></h5>
-	  <div class="pBody">
-	    <ul>
-	      <?php foreach($this->data['language_urls'] as $langlink) { ?>
-	      <li class="<?php echo htmlspecialchars($langlink['class'])?>">
-	      <a href="<?php echo htmlspecialchars($langlink['href'])
-	        ?>"><?php echo $langlink['text'] ?></a>
-	      </li>
-	      <?php } ?>
-	    </ul>
-	  </div>
-	</div>
-	<?php } ?>
-    <?php if (false) { ?>
-      </div><!-- end of the left (by default at least) column -->
-      <div class="visualClear"></div>
-      <div id="footer">
-    <?php if($this->data['poweredbyico']) { ?><div id="f-poweredbyico"><?php $this->html('poweredbyico') ?></div><?php } ?>
-	<?php if($this->data['copyrightico']) { ?><div id="f-copyrightico"><?php $this->html('copyrightico') ?></div><?php } ?>
-	<ul id="f-list">
-	  <?php if($this->data['lastmod'   ]) { ?><li id="f-lastmod"><?php    $this->html('lastmod')    ?></li><?php } ?>
-	  <?php if($this->data['viewcount' ]) { ?><li id="f-viewcount"><?php  $this->html('viewcount')  ?></li><?php } ?>
-	  <?php if($this->data['numberofwatchingusers' ]) { ?><li id="f-numberofwatchingusers"><?php  $this->html('numberofwatchingusers') ?></li><?php } ?>
-	  <?php if($this->data['credits'   ]) { ?><li id="f-credits"><?php    $this->html('credits')    ?></li><?php } ?>
-	  <?php if($this->data['copyright' ]) { ?><li id="f-copyright"><?php  $this->html('copyright')  ?></li><?php } ?>
-	  <?php if($this->data['about'     ]) { ?><li id="f-about"><?php      $this->html('about')      ?></li><?php } ?>
-	  <?php if($this->data['disclaimer']) { ?><li id="f-disclaimer"><?php $this->html('disclaimer') ?></li><?php } ?>
-	  <?php if($this->data['tagline']) { ?><li id="f-tagline"><?php echo $this->data['tagline'] ?></li><?php } ?>
-	</ul>
-      </div>
-    </div>
-    <?php } ?>
-    <?php $this->html('reporttime') ?>
+		<h5><?php $this->msg('toolbox') ?></h5>
+		<div class="pBody">
+			<ul>
 <?php
-	wfRestoreWarnings();
-    include($_SERVER['DOCUMENT_ROOT']."/footer.html");
-	}
-}
+		if($this->data['notspecialpage']) { ?>
+				<li id="t-whatlinkshere"><a href="<?php
+				echo htmlspecialchars($this->data['nav_urls']['whatlinkshere']['href'])
+				?>"><?php $this->msg('whatlinkshere') ?></a></li>
+<?php
+			if( $this->data['nav_urls']['recentchangeslinked'] ) { ?>
+				<li id="t-recentchangeslinked"><a href="<?php
+				echo htmlspecialchars($this->data['nav_urls']['recentchangeslinked']['href'])
+				?>"><?php $this->msg('recentchangeslinked') ?></a></li>
+<?php 		}
+		}
+		if(isset($this->data['nav_urls']['trackbacklink'])) { ?>
+			<li id="t-trackbacklink"><a href="<?php
+				echo htmlspecialchars($this->data['nav_urls']['trackbacklink']['href'])
+				?>"><?php $this->msg('trackbacklink') ?></a></li>
+<?php 	}
+		if($this->data['feeds']) { ?>
+			<li id="feedlinks"><?php foreach($this->data['feeds'] as $key => $feed) {
+					?><span id="feed-<?php echo Sanitizer::escapeId($key) ?>"><a href="<?php
+					echo htmlspecialchars($feed['href']) ?>"><?php echo htmlspecialchars($feed['text'])?></a>&nbsp;</span>
+					<?php } ?></li><?php
+		}
 
+		foreach( array('contributions', 'blockip', 'emailuser', 'upload', 'specialpages') as $special ) {
+
+			if($this->data['nav_urls'][$special]) {
+				?><li id="t-<?php echo $special ?>"><a href="<?php echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
+				?>"><?php $this->msg($special) ?></a></li>
+<?php		}
+		}
+
+		if(!empty($this->data['nav_urls']['print']['href'])) { ?>
+				<li id="t-print"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['print']['href'])
+				?>"><?php $this->msg('printableversion') ?></a></li><?php
+		}
+
+		if(!empty($this->data['nav_urls']['permalink']['href'])) { ?>
+				<li id="t-permalink"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['permalink']['href'])
+				?>"><?php $this->msg('permalink') ?></a></li><?php
+		} elseif ($this->data['nav_urls']['permalink']['href'] === '') { ?>
+				<li id="t-ispermalink"><?php $this->msg('permalink') ?></li><?php
+		}
+
+		wfRunHooks( 'UKCODTemplateToolboxEnd', array( &$this ) );
+?>
+			</ul>
+		</div>
+<? */ ?>
+	</div>
+<?php
+		if( $this->data['language_urls'] ) { ?>
+	<div id="p-lang" class="portlet">
+		<h5><?php $this->msg('otherlanguages') ?></h5>
+		<div class="pBody">
+			<ul>
+<?php		foreach($this->data['language_urls'] as $langlink) { ?>
+				<li class="<?php echo htmlspecialchars($langlink['class'])?>"><?php
+				?><a href="<?php echo htmlspecialchars($langlink['href']) ?>"><?php echo $langlink['text'] ?></a></li>
+<?php		} ?>
+			</ul>
+		</div>
+	</div>
+<?php	} ?>
+		</div><!-- end of the left (by default at least) column -->
+			<div class="visualClear"></div>
+			<div id="footer">
+<?php
+/*		if($this->data['poweredbyico']) { ?>
+				<div id="f-poweredbyico"><?php $this->html('poweredbyico') ?></div>
+<?php 	}
+		if($this->data['copyrightico']) { ?>
+				<div id="f-copyrightico"><?php $this->html('copyrightico') ?></div>
+<?php	} */
+
+		// Generate additional footer links
+?>
+			<ul id="f-list">
+<?php
+		$footerlinks = array(
+		#	'lastmod', 'viewcount', 'numberofwatchingusers', 'credits', 'copyright',
+		#	'privacy', 'about', 'disclaimer', 'tagline',
+        'lastmod'
+		);
+		foreach( $footerlinks as $aLink ) {
+			if( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
+?>				<li id="<?php echo$aLink?>"><?php $this->html($aLink) ?></li>
+<?php 		}
+		}
+?>
+			</ul>
+		</div>
+		
+	<?php $this->html('bottomscripts'); /* JS call to runBodyOnloadHook */ ?>
+</div>
+<?php $this->html('reporttime') ?>
+<?php if ( $this->data['debug'] ): ?>
+<!-- Debug output:
+<?php $this->text( 'debug' ); ?>
+
+-->
+<?php endif;
+    include($_SERVER['DOCUMENT_ROOT']."/header.html");
+	wfRestoreWarnings();
+	} // end of execute() method
+} // end of class
 ?>
