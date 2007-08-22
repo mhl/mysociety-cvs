183225dfadf568bc9dc68a6fa9e44f21fa9342e5
mswww-staging <mswww-staging>
2007-08-22 14:16:13 +0000

    Job advert

---
+++ ms/web/2007/job-advert/index.php
@@ -0,0 +1,62 @@
+<?php
+include "../../wordpress/wp-blog-header.php";
+include "../../wordpress/wp-content/themes/mysociety/header.php"; 
+
+# Needs reorganising to have more important things at the top
+?>
+
+<div id="betteritemdiv">
+
+<h2>mySociety is hiring an open source web developer</h2>
+
+<p>mySociety is an award-winning charitable project building websites that deliver
+simple, tangible benefits to people in the UK. Since 2003, we have set up many
+of the UK's best-known internet democracy websites, including <a href="http://www.theyworkforyou.com">TheyWorkForYou</a>,
+<a href="http://www.fixmystreet.com">FixMyStreet</a>, <a href="http://www.pledgebank.com">PledgeBank</a>, and the 
+<a href="http://petitions.pm.gov.uk">10 Downing Street e-petitions site</a>.
+
+<p>We are now looking to hire a web developer with exceptional user interface
+design skills. This is a rare opportunity for an outstanding individual to join
+our small team of paid developers, work on ground-breaking new projects, and
+help shape the future of both mySociety and our democracy.
+
+<h3>Technical skills</h3>
+<ul>
+<li>World-class skills in one or more of PHP, Perl, Python or Ruby
+<li>Expertise in open source operating systems, databases, and version control
+<li>Experience of working in a team with other programmers, contributing to project architecture
+<li>Experience of accessible, standards-based web design
+<li>Fanatical obsession with usability
+</ul>
+
+<h3>Personal qualities</h3>
+
+<ul>
+<li>Self-starter, with the ability to manage own work and to work both at home and with colleagues
+<li>'Completer-finisher' personality, with track record of delivering projects to deadline
+<li>Team player with strong communication skills
+<li>Personal enthusiasm for mySociety's goals - see <a href="http://www.mysociety.org/faq">our FAQ</a> for more details 
+</ul>
+
+<h3>Benefits and Salary</h3>
+
+<ul>
+<li>Competitive salary
+<li>Opportunity to contribute to high-profile sites delivering real democratic and community benefits to citizens
+<li>Opportunities to travel to political and media events in the UK and abroad
+<li>mySociety is a virtual organisation, so hours and location are highly flexible
+<li>Minimum 12 month commitment after probationary period
+</ul>
+
+<p><strong>To apply, please send a CV and covering letter to <a
+href="mailto:tom@mysociety.org">Tom Steinberg</a>, the Director of
+mySociety, before 10th September 2007. For more information about the role or
+mySociety, contact Tom on +44 7811 082158.</strong>
+
+
+
+
+</div>
+
+<?php include "wordpress/wp-content/themes/mysociety/footer.php"; ?>
+
