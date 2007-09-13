8044227e37945b0464d3d539800e2f09b35f7c67
mswww-staging <mswww-staging>
2007-09-13 16:31:03 +0000

    Tweak projects page.

--- ms/web/projects.php
+++ ms/web/projects.php
@@ -3,129 +3,104 @@ include "wordpress/wp-blog-header.php";
 include "wordpress/wp-content/themes/mysociety/header.php"; 
 ?>
 
-<div class="item_head">
-Launched sites
-</div>
+<div class="itembox">
+<h1>Launched sites</h1>
 
-<div class="item_inner_head">
-<a href="http://www.fixmystreet.com/">FixMyStreet &ndash; launched 7th March 2007</a>
-</div>
-<div class="item">
-A site where people can report, view, or discuss local problems like graffiti,
-fly tipping, broken paving slabs, or street lighting. We're making this 
+<h2><a href="http://www.fixmystreet.com/">FixMyStreet</a> <small>(launched 7th March 2007)</small></h2>
+<p>A site where people can report, view, or discuss local problems like graffiti,
+fly tipping, broken paving slabs, or street lighting. Made
 in collaboration with the <a href="http://www.youngfoundation.org.uk/">Young
-Foundation</a>. (Was originally called Neighbourhood Fix-It.)
-</div>
+Foundation</a>, and originally called Neighbourhood Fix-It.
+</p>
 
-<div class="item_inner_head">
-<a href="http://www.hearfromyourmp.com">HearFromYourMP.com &ndash; launched 21st November 2005</a>
-</div>
-<div class="item">
+<h2><a href="http://www.hearfromyourmp.com">HearFromYourMP</a> <small>(launched 21st November 2005)</small></h2>
+<p>
 HearFromYourMP encourages and enables MPs to run email lists for their
 constituents, and to allow those constituents to discuss ideas in a way which
-doesn't bombard them with email.  5000 people had signed up before it was even
+doesn't bombard them with email.  5,000 people had signed up before it was even
 launched.
-</div>
+</p>
 
-<div class="item_inner_head">
-<a href="http://www.pledgebank.com">PledgeBank.com &ndash; launched 13th June 2005</a>
-</div>
-<div class="item">
+<h2><a href="http://www.pledgebank.com">PledgeBank</a> <small>(launched 13th June 2005)</small></h2>
+<p>
 PledgeBank is about reassuring people who want to do something altruistic or
 socially beneficially that they won't be alone in their actions. It lets users
 create pledges which say "I'll do something, but only if 10 other people will
 do something", for example "I'll clean up the banks of my local river, but only
 if 5 other local people will pledge to come and help.
-</div>
+</p>
 
-<div class="item_inner_head">
-<a href="http://www.notapathetic.com">NotApathetic.com &ndash; launched 7th April 2005</a>
-</div>
-<div class="item">
+<h2><a href="http://www.notapathetic.com">NotApathetic</a> <small>(launched 7th April 2005)</small></h2>
+<p>
 NotApathetic was built so that people who were planning not to vote in
 the UK General Election on May 5th 2005 had the chance to tell the
 world why. Non-judgmental and non-partisan NotApathetic caught the
 attention of 40,000 visitors during the general election, and was
 discussed on a dozen local radio stations, and in newspapers from New
 Zealand to South Africa. 
-</div>
+</p>
 
-<div class="item_inner_head">
-<a href="http://www.writetothem.com">WriteToThem.com &ndash; launched 14th February 2005</a>
-</div>
-<div class="item">
+<h2><a href="http://www.writetothem.com">WriteToThem</a> <small>(launched 14th February 2005)</small></h2>
+<p>
 WriteToThem.com is the definitive place to contact any of your elected
 representatives. Enter a single postcode and it'll tell you who all
 your local representatives are, and a bit about who you should contact
 for which reasons. An award winner within 12 weeks of launching,
 WriteToThem.com sent 5,000 messages in its first month of operation.
-</div>
+</p>
 
-<div class="item_inner_head">
-<a href="http://www.theyworkforyou.com">TheyWorkForYou.com &ndash; launched 6th June 2004</a>
-</div>
-<div class="item">
+<h2><a href="http://www.theyworkforyou.com">TheyWorkForYou</a> <small>(launched 6th June 2004)</small></h2>
+<p>
 TheyWorkForYou provides a searchable, annotatable version of what is said in
 Parliament, as well as useful pages providing clear, non-biased information on
 a range of different measures of activities by MPs.  Originally built by
 volunteers while mySociety was getting started, it is now part of mySociety.
-</div>
-<div class="item_foot">
+</p>
+
 </div>
 
 <!--<div class="item_head">
 Sites In Testing
 </div>-->
 
-<div class="item_head">
-Sites we built for other people
-</div>
+<div class="itembox">
 
-<div class="item_inner_head">
-<a href="http://petitions.pm.gov.uk">E-Petitions &ndash; launched 14th November 2006</a>
-</div>
-<div class="item">
+<h1>Sites we built for other people</h1>
+
+<h2><a href="http://petitions.pm.gov.uk">E-Petitions</a> <small>(launched 14th November 2006)</small></h2>
+<p>
 We built the 10 Downing Street e-Petitions site as a civil service commissioned
 project to allow members of the public to petition the Prime Minister about
 whatever issues they see fit. Highly robust and load-tolerant, it is also
 available as open source code for reuse elsewhere.  
-</div>
+</p>
 
-<div class="item_foot">
 </div>
+<div class="itembox">
 
+<h1>Sites being built</h1>
 
-<div class="item_head">
-Sites being built
-</div>
-
-<div class="item_inner_head">
-Freedom of Information Filer and Archive
-</div>
-<div class="item">
+<h2>Freedom of Information Filer and Archive</h2>
+<p>
 We're building a website to help people make Freedom of Information requests
 from different parts of government. It will then archive the responses on the
 web. We've got <a
 href="http://www.mysociety.org/2006/12/06/funding-for-freedom-of-information/">funding
 from one of Joseph Rowntree's trusts</a> for this (yes, the dead chocolate mogul;
 eat lots of chocolate, it'll help mySociety's successors in the 22nd century).
-</div>
-<div class="item_foot">
-</div>
+</p>
 
-<div class="item_head">
-What now?
 </div>
-<div class="item">
+<div class="itembox">
+
+<h1>What now?</h1>
 <ul>
 <li>We appreciate feedback on all our live sites, so please <a href="mailto:team&#64;mysociety.org">tell us</a> what we can improve.</li>
 <li>Non-techy, graphic designer, programmer? <a href="/volunteertasks">Help build, publicise and improve</a> our sites.</li>
 <li>If you're a techy, you might be interested in our <a href="https://secure.mysociety.org/cvstrac/dir?d=mysociety">source code</a>.
 </ul>
-</div>
-<div class="item_foot">
-</div>
 
+</div>
 
 <?php include "wordpress/wp-content/themes/mysociety/footer.php"; ?>
 
