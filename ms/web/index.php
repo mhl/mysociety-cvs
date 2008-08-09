44d6283ab0260fb818fdd5fd590d6515df1dcb20
angie <angie>
2008-08-09 17:38:00 +0000

    adding new wp folder

--- ms/web/index.php
+++ ms/web/index.php
@@ -1,8 +1,8 @@
 <?php 
 if (empty($_GET['cat'])) {
-    $cat = 1;
+   $cat = 1;
 }
 /* Short and sweet */
 define('WP_USE_THEMES', true);
-require('wordpress/wp-blog-header.php');
+require('./wordpress/wp-blog-header.php');
 ?>
