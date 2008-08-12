69bbfd9283e362da783ab126cb0b9cdc025b3d7f
angie <angie>
2008-08-12 14:11:27 +0000

    staging switch

--- ms/web/index.php
+++ ms/web/index.php
@@ -1,8 +1,24 @@
 <?php 
-//if (empty($_GET['cat'])) {
-//   $cat = 1;
-//}
-/* Short and sweet */
+if (!@include "../conf/general") {
+    if (!@include "../../conf/general") {
+        if (!@include "../../../conf/general") {
+            print "Error including conf/general in wp-config.php on mysociety.org WordPress";
+            exit;
+        }
+    }
+}
 define('WP_USE_THEMES', true);
-require('./wp/wp-blog-header.php');
+
+if (OPTION_STAGING == '1') {
+    require('./wp/wp-blog-header.php');
+    print "staging";
+} else {
+    if (empty($_GET['cat'])) {
+        $cat = 1;
+    }
+/* Short and sweet */
+    require('./wordpress/wp-blog-header.php');    
+    print "live";
+}
+
 ?>
