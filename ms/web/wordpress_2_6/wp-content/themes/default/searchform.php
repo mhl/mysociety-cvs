d3719588c8f141df85a8cca4d054557b06f77edc
angie <angie>
2008-08-12 18:32:38 +0000

    only one thing per id angie ;)

--- ms/web/wordpress_2_6/wp-content/themes/default/searchform.php
+++ ms/web/wordpress_2_6/wp-content/themes/default/searchform.php
@@ -1,4 +1,4 @@
-<div id="searchform">
+<div id="searchdiv">
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 <label class="hidden" for="s"><?php _e('Search for:'); ?></label>
 <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
