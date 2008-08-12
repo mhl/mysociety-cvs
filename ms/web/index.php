3a74082d48bcdb29ef87d910e5dabd507ab20e08
angie <angie>
2008-08-12 15:30:53 +0000

    index

--- ms/web/index.php
+++ ms/web/index.php
@@ -11,14 +11,12 @@ define('WP_USE_THEMES', true);
 
 if (OPTION_STAGING == '1') {
     require('./wp/wp-blog-header.php');
-    print "staging";
 } else {
     if (empty($_GET['cat'])) {
         $cat = 1;
     }
 /* Short and sweet */
     require('./wordpress/wp-blog-header.php');    
-    print "live";
 }
 
 ?>
