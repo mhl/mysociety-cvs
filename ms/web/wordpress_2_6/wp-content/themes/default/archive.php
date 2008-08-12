c335cab852bac17061d281e6afa74a9ef4bf6185
angie <angie>
2008-08-12 19:52:22 +0000

    sort projects

--- ms/web/wordpress_2_6/wp-content/themes/default/archive.php
+++ ms/web/wordpress_2_6/wp-content/themes/default/archive.php
@@ -26,7 +26,10 @@
 			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
 			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
 		</div>
-
+<?php if (is_category('Projects')) {
+     $posts = query_posts($query_string . '&orderby=title&order=asc&posts_per_page=-1');
+} 
+?>
 		<?php while (have_posts()) : the_post(); ?>
 		<div class="post">
 				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
