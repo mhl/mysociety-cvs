556b8ff1c69015ba68a2ea5263ef23da2fb2f531
angie <angie>
2008-08-15 16:24:49 +0000

    path to footer file

--- ms/web/wordpress_2_6/wp-content/themes/default/footer.php
+++ ms/web/wordpress_2_6/wp-content/themes/default/footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 <?
-    print file_get_contents("footer.html"); 
+    print file_get_contents($_SERVER['DOCUMENT_ROOT'] .  "/footer.html"); 
 ?>
 
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
