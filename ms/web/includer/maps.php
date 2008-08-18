e851cd0dcf904b0add9c7589792558095203cdf8
richard <richard>
2008-08-18 08:57:06 +0000

    moved product pages to includer

---
+++ ms/web/includer/maps.php
@@ -0,0 +1,78 @@
+<div id="divProductHeadline" class="contenthalf aligncenter">
+
+	<!-- Header -->
+	<h1>Commuting Time Maps</h1>
+	<p>
+		A World First - Custom maps that help house hunters and office managers find the best places quick and for easy commutes
+	</p>
+
+	<!-- More info -->
+	<a class="linkbutton" href="#">
+        <span class="left">&nbsp;</span>
+        <span class="middle">Get a quote for your map now</span>                
+        <span class="right">&nbsp;</span>
+    </a>
+</div>
+
+<div class="contenthalf aligncenter">
+	<img src="images/traveltime.png" alt="Travel time to Department of Transport" title="Travel time to Department of Transport" width="300px" height="300px"/>
+</div>
+
+<br class="clear" />
+
+<!-- Short paragraph about the system-->
+<div class="contentfull">
+	<div class="dividertop dividerbottom">
+		<p class="aligncenter">
+			<strong>
+				<span class="highlight">
+				Get your own interactive or static maps of commuting times for anywhere in the UK
+			</span>
+		</strong>
+		</p>
+	</div>
+</div>
+
+<!-- Features -->
+<div class="contentwide left">
+
+	<h4>
+	House hunting? Find the right areas for an easy commute</h4>
+	<p>
+		No more stressing over the tube map, trying to work out where you can live an still get to work in less than 25 minutes - we'll make a map that shows you only the areas that match your acceptable criteria.
+		
+	</p>
+	
+	<h4>Choose your preferred form of transport</h4>
+	<p>
+		Want to get to work by any form of transport except the tube or train? No problem. Want to know the quickest way by any means, we've got you covered.
+	</p>
+	
+	<h4>Relocating an office?</h4>
+	<p>
+		Want to move offices? Wondering how it will affect your staff commutes? Get in touch and we'll get you the maps and statistics you need
+	</p>
+
+</div>
+<div class="contentnarrow right">
+	
+	<!-- Reasons to buy -->
+	<ul class="ticklist">
+		<li>Driving</li>
+		<li>Trains</li>
+		<li>Tubes</li>				
+		<li>Boats</li>							
+		<li>Buses</li>												
+		<li>Trams</li>	
+		<li>All of the UK</li>	
+	</ul>
+	
+	<!--- Quote 2 -->
+	<div class="infobox">
+		<blockquote>
+			"This is a really cool idea"
+		</blockquote>
+		<cite>- Randall Munroe, <a href="http://xkcd.com/">XKCD</a></cite>
+	</div>
+</div>
+<br class="clear"/>
