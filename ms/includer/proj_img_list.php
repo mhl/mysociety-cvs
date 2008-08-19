a41c58dcc52b21cebfaa9d437849e3a18d84c530
angie <angie>
2008-08-19 17:26:19 +0000

    project list with images

---
+++ ms/includer/proj_img_list.php
@@ -0,0 +1,29 @@
+<?php
+query_posts('cat=7&orderby=title&order=asc&posts_per_page=-1');
+$pages = get_pages('child_of=403&echo=0');
+    foreach ($pages as $page_obj) {
+        $page = get_object_vars($page_obj);
+        $url = '/' .  get_page_uri($page['ID']);
+        $url;
+        //$keys = array_keys($page);
+        //print_r($keys);
+        $imageurl = '';
+        if (file_exists(dirname(__FILE__) . '/../web/contimg/projtns/' . strtolower($page[post_title]) . '.jpg')) {
+            $imageurl = '/contimg/projtns/' . strtolower($page[post_title]) . '.jpg';
+        }
+
+?> 
+
+<?php  if ($imageurl) { 
+
+?>
+<div class="wp-caption alignleft" style="width: 129px"><a href="<?php print $url;?>">
+<img src="<?php print $imageurl ?>" alt="image of <?php print $page[post_title]; ?>" title="<?php print $page[post_title]; ?>" width="125" height="126" class="size-thumbnail" border="0"/></a>
+<p class="wp-caption-text"><?php print $page[post_title]; ?></p></div>
+<?php } 
+} // end projects list
+
+ ?>
+
+
+
