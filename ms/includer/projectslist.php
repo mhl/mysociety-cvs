00c0af4f1e8c49bfba1fe25bff6c15c59f0dca6b
angie <angie>
2008-08-19 17:00:58 +0000

    project list

--- ms/includer/projectslist.php
+++ ms/includer/projectslist.php
@@ -1,15 +1,21 @@
 <?php
 query_posts('cat=7&orderby=title&order=asc&posts_per_page=-1');
+$pages = get_pages('child_of=403&echo=0');
+    foreach ($pages as $page_obj) {
+        $page = get_object_vars($page_obj);
+        $url = '/' .  get_page_uri($page['ID']);
+        $url;
+        //$keys = array_keys($page);
+        //print_r($keys);
+?> 
+<h4><a href="<?php print $url;?>"><?php print $page[post_title]; ?></a></h4>
+<p><?php print $page[post_excerpt]; ?></p>
+
+
+<?php  
+} // end projects list
+
  ?>
- 
-<?php while (have_posts()) : the_post(); ?>
-<div class="post">
-        <h3 id="post-<?php the_ID(); ?>">
-        <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 
-        <div class="excerpt">
-            <?php the_excerpt(); ?>
-        </div>
 
-    </div>
-<?php endwhile; ?>
+
