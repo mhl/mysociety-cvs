fb4f546c9fe41e5a60b81e325fd7171f261277c9
angie <angie>
2008-08-20 18:54:51 +0000

    new index

--- ms/web/index.php
+++ ms/web/index.php
@@ -9,16 +9,6 @@ if (!@include "../conf/general") {
 }
 define('WP_USE_THEMES', true);
 
-if (OPTION_STAGING) {
-    // new site
-    require('./wp/wp-blog-header.php');
-} else {
-    // old site XXX remove this in the end
-    if (empty($_GET['cat'])) {
-        $cat = 1;
-    }
-    /* Short and sweet */
-    require('./wordpress/wp-blog-header.php');    
-}
+require('./wp/wp-blog-header.php');
 
 ?>
