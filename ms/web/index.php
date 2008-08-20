7affb2deb898e4edcfe204c0e5eabb4a48dfa532
francis <francis>
2008-08-20 09:07:49 +0000

    Don't rely on staging variable being a string. Some comments.

--- ms/web/index.php
+++ ms/web/index.php
@@ -9,13 +9,15 @@ if (!@include "../conf/general") {
 }
 define('WP_USE_THEMES', true);
 
-if (OPTION_STAGING == '1') {
+if (OPTION_STAGING) {
+    // new site
     require('./wp/wp-blog-header.php');
 } else {
+    // old site XXX remove this in the end
     if (empty($_GET['cat'])) {
         $cat = 1;
     }
-/* Short and sweet */
+    /* Short and sweet */
     require('./wordpress/wp-blog-header.php');    
 }
 
