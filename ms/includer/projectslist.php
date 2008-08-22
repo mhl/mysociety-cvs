8ea51079c1ecc9ad5eea9c9934158caa8093f268
matthew <matthew>
2008-08-22 15:39:32 +0000

    Call a list a list; don't underline headings even if they're links, don't apply margin to inline element (don't think that affects anything).

--- ms/includer/projectslist.php
+++ ms/includer/projectslist.php
@@ -2,20 +2,11 @@
 //query_posts('cat=7&orderby=title&order=asc&posts_per_page=-1');
 
 $pages = get_pages('child_of=403&echo=0');
-    foreach ($pages as $page_obj) {
-        $page = get_object_vars($page_obj);
-        $url = '/' .  get_page_uri($page['ID']);
-        $url;
-        //$keys = array_keys($page);
-        //print_r($keys);
-?> 
-<h4><a href="<?php print $url;?>"><?php print $page[post_title]; ?></a></h4>
-
-
-<?php  
+echo '<ul id="project_list">';
+foreach ($pages as $page_obj) {
+    $page = get_object_vars($page_obj);
+    $url = '/' .  get_page_uri($page['ID']) . '/';
+    echo '<li><a href="', $url, '">', $page['post_title'], '</a></li>';
 } // end projects list
-
- ?>
-
-
+echo '</ul>';
 
