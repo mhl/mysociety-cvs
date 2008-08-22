f185c447b4cf7712a1b3eaabf29a2c21add73c63
matthew <matthew>
2008-08-22 11:49:03 +0000

    Fix broken pages; clean up project page a bit.

--- ms/includer/proj_img_list.php
+++ ms/includer/proj_img_list.php
@@ -3,28 +3,27 @@
 $pages = get_pages('child_of=403&echo=0');
     foreach ($pages as $page_obj) {
         $page = get_object_vars($page_obj);
-        $url = '/' .  get_page_uri($page['ID']);
-        $url;
+        $url = '/' .  get_page_uri($page['ID']) . '/';
         //$keys = array_keys($page);
         //print_r($keys);
         $imageurl = '';
-        if (file_exists(dirname(__FILE__) . '/../web/contimg/projtns/' . strtolower($page[post_title]) . '.jpg')) {
-            $imageurl = '/contimg/projtns/' . strtolower($page[post_title]) . '.jpg';
+        if (file_exists(dirname(__FILE__) . '/../web/contimg/projtns/' . strtolower($page['post_title']) . '.jpg')) {
+            $imageurl = '/contimg/projtns/' . strtolower($page['post_title']) . '.jpg';
         }
 
 ?> 
 
-<?php  if ($imageurl) { 
+<?php  if ($imageurl) {
 
 ?>
-<div class="wp-caption alignleft" style="width: 129px"><a href="<?php print $url;?>">
-<img src="<?php print $imageurl ?>" alt="image of <?php print $page[post_title]; ?>" title="<?php print $page[post_title]; ?>" width="125" height="126" class="size-thumbnail" border="0"/></a>
-<p class="wp-caption-text"><?php print $page[post_title]; ?></p></div>
+<div class="projects-entry">
+<div class="wp-caption alignleft"><a href="<?php print $url;?>"><img src="<?php print $imageurl ?>" alt="" title="<?php print $page['post_title']; ?> screenshot" width="125" height="126" class="size-thumbnail" border="0" /></a>
+</div>
 
-<h4><a href="<?php print $url;?>"><?php print $page[post_title]; ?></a></h4>
-<?php print $page[post_excerpt]; ?>
+<h2><a href="<?php print $url;?>"><?php print $page['post_title']; ?></a></h2>
+<?php print $page['post_excerpt']; ?>
+</div>
 
-<hr />
 <?php } 
 } // end projects list
 
