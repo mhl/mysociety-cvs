30132730426d00ce766e1fb7a1d5213d0d44078b
matthew <matthew>
2008-08-22 12:26:15 +0000

    Drop duplicate link on projects; 100% still has tiny horizontal scrollbar so drop for now.

--- ms/includer/proj_img_list.php
+++ ms/includer/proj_img_list.php
@@ -17,11 +17,8 @@ $pages = get_pages('child_of=403&echo=0');
 
 ?>
 <div class="projects-entry">
-<div class="wp-caption alignleft"><a href="<?php print $url;?>"><img src="<?php print $imageurl ?>" alt="" title="<?php print $page['post_title']; ?> screenshot" width="125" height="126" class="size-thumbnail" border="0" /></a>
-</div>
-
-<h2><a href="<?php print $url;?>"><?php print $page['post_title']; ?></a></h2>
-<?php print $page['post_excerpt']; ?>
+<h2><a href="<?=$url ?>"><img src="<?php print $imageurl ?>" alt="" title="<?php print $page['post_title']; ?> screenshot" width="125" height="126" class="size-thumbnail" border="0" /><?=$page['post_title'] ?></a></h2>
+<?=$page['post_excerpt'] ?>
 </div>
 
 <?php } 
