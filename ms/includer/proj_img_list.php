81a50255588f5b68721339c19d26338b3fc7b899
francis <francis>
2008-08-27 13:02:35 +0000

    Better order.

--- ms/includer/proj_img_list.php
+++ ms/includer/proj_img_list.php
@@ -1,6 +1,6 @@
 <?php
 //query_posts('cat=7&orderby=title&order=asc&posts_per_page=-1');
-$pages = get_pages('child_of=403&echo=0');
+$pages = get_pages('child_of=403&echo=0&sort_column=menu_order');
     foreach ($pages as $page_obj) {
         $page = get_object_vars($page_obj);
         $url = '/' .  get_page_uri($page['ID']) . '/';
