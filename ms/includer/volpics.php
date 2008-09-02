4d17406f9a8c945762499fcfee598557b4a83449
angie <angie>
2008-09-02 13:43:35 +0000

    volunteer pics for home page to make sams happy :)

---
+++ ms/includer/volpics.php
@@ -0,0 +1,18 @@
+<a href="/helpus/"><img src="/contimg/volpics/mysoc-opentech.jpg" alt=" " id="randomphoto" /></a>
+<script type="text/javascript">
+
+   var maxlink=4;
+
+   var frontpage_images=new Array(maxlink);
+   frontpage_images[0]= "/contimg/volpics/away-weekend.jpg"; 
+   frontpage_images[1]= "/contimg/volpics/devs-with-laptops.jpg"; 
+   frontpage_images[2]= "/contimg/volpics/mysoc-opentech.jpg"; 
+   frontpage_images[3]= "/contimg/volpics/sunlightweekend.jpg"; 
+   frontpage_images[4]= "/contimg/volpics/tarn.jpg"; 
+
+
+   var showpic= Math.ceil(Math.random() * ( 1 + maxlink ) ) - 1 ;
+
+   document.getElementById('randomphoto').src=frontpage_images[showpic]; 
+
+</script>
