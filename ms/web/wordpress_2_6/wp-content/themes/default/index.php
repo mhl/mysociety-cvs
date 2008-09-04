7af92ec306fb8769783ab89d4023275888632641
richard <richard>
2008-09-04 14:50:30 +0000

    homepage template

--- ms/web/wordpress_2_6/wp-content/themes/default/index.php
+++ ms/web/wordpress_2_6/wp-content/themes/default/index.php
@@ -1,46 +1,4 @@
 <?php get_header(); ?>
 
-<!--Hero-->
-<div id="divHero" class="infoboxpurple contentfull">
-	<img src="/contimg/sitesanimation.gif" alt="TheyWorkForYou, WriteToThem and GroupsNearYou" title="TheyWorkForYou, WriteToThem and GroupsNearYou" width="349" height="173"/>
-	<h3>Hello, we are mySociety and we run most of 
-	the UK’s best known democracy websites.</h3>
-	<p>
-		Helped by our services, <strong>200,000</strong> people have written to their MP 
-		for the first time, fixed <strong>8,000</strong> potholes and other broken things, petitioned the Prime Minister <strong>2,000,000</strong> times and knitted at least <strong>37</strong> tiny hats for charity.
-		<br/>
-		<a href="#" class="morelink">View all our projects &raquo;</a>
-	</p>
-	<span class="infobottom">&nbsp;</span>
-</div>
-
-<!-- Get involved -->
-<div class="contenthalf left infoboxgreen">
-	<h3>Get involved</h3>
-	<p>mySociety is built on volunteers. From simple things like playing a game to help us make sense of video from the House of Commons, or reporting a broken paving slab, to building a new service our data or helping us look after on of our websites.</p>
-	
-	<p>
-		Or just joining our discussion groups and telling us how to make stuff better.
-		<br/>
-		<a href="#" class="morelink">See what you could do &raquo;</a>
-	</p>
-	<span class="infobottom">&nbsp;</span>	
-</div>
-
-<!-- Hire us -->
-<div class="contenthalf right infoboxblue">
-	<h3>Hire us</h3>
-	<p>
-		If you work in the public, private or third sector, and are trying to adapt to the new world of 
-		citizens empowering themselves through the internet, mySociety can help you by building sites, consulting or helping you plan. 
-	</p>
-	<p>
-		We’ve worked for No10, the BBC and Google, and we’d love to work for you.
-		<br/>
-		<a href="#" class="morelink">Find out more &raquo;</a>
-	</p>
-	<span class="infobottom">&nbsp;</span>	
-</div>
-<br class="clear"/>
 
 <?php get_footer(); ?>
