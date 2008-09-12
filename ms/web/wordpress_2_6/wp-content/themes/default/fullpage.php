48634f67f83674cb5db0818957d5b02498d7504e
richard <richard>
2008-09-12 10:06:39 +0000

    header on template

--- ms/web/wordpress_2_6/wp-content/themes/default/fullpage.php
+++ ms/web/wordpress_2_6/wp-content/themes/default/fullpage.php
@@ -6,6 +6,7 @@ Template Name: Full width
 
 <?php get_header(); ?>
 
+	<h1><?php the_title(); ?></h1>
 	<div class="contentfull">
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
