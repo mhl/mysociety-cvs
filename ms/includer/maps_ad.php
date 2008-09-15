ceb76cf4873ef356170672510a06e0650269f2b4
angie <angie>
2008-09-15 12:09:53 +0000

    maps ad

---
+++ ms/includer/maps_ad.php
@@ -0,0 +1,14 @@
+<div id="divProductHeadline" class="contenthalf aligncenter">
+	<!-- Header -->
+	<h1>Commuting Time Maps</h1>
+	<p>
+		A World First - Custom maps that help house hunters and office managers find the best places quick and for easy commutes
+	</p>
+
+	<!-- More info -->
+	<a class="linkbutton" href="/services/request-a-quote/?p=map">
+        <span class="left">&nbsp;</span>
+        <span class="middle">Get a quote for your map now</span>                
+        <span class="right">&nbsp;</span>
+    </a>
+</div>
