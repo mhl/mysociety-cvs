2d7938d178f830c02894afa96747b62f0a09920f
francis <francis>
2009-03-26 07:53:10 +0000

    Don't try storing the rows in the database, pointless as can index from the station table into a binary file.

--- iso/db/drop-tables.sql
+++ iso/db/drop-tables.sql
@@ -1,6 +1,5 @@
 -- useful for Contours of Life, as tables are all temporary
 
-drop table place_time;
 drop table map;
 drop table station;
 
--- iso/db/schema.sql
+++ iso/db/schema.sql
@@ -1,17 +1,3 @@
--- schema.sql:
--- Schema for Contours of Life database.
---
--- Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
--- Email: francis@mysociety.org; WWW: http://www.mysociety.org/
---
--- $Id: schema.sql,v 1.5 2009-03-25 15:49:52 francis Exp $
---
-
--- Separately from this Schema, you need to set up PostGIS in 
--- the database. You must do the following, as the postgres database
--- user for the permission needed.
-
--- Enable scripting:
 -- CREATE FUNCTION plpgsql_call_handler()
 --         RETURNS OPAQUE AS '$libdir/plpgsql' LANGUAGE 'C';
 -- CREATE TRUSTED PROCEDURAL LANGUAGE 'plpgsql' HANDLER plpgsql_call_handler
@@ -32,7 +18,7 @@ create table station (
     connectedness   integer not null,
     minimum_zoom    integer default 0
 );
-create unique index station_text_id_idx on station(text);
+create unique index station_text_id_idx on station(text_id);
 create index station_minimum_zoom_idx on station(minimum_zoom);
 
 -- SRID 27700 = OSGB 1936 / British National Grid
@@ -69,21 +55,6 @@ create table map (
 
 --  target_station integer not null references station(id)
 );
-
--- For each map
-create table place_time (
-    map_id integer not null references map(id),
-
-    minutes_to_target integer not null,
-    minimum_zoom integer default 0
-);
--- SRID 27700 = OSGB 1936 / British National Grid
-select AddGeometryColumn('', 'place_time', 'position_osgb', 27700, 'POINT', 2);
--- SRID 900913 = Spherical mercator
-select AddGeometryColumn('', 'place_time', 'position_merc', 900913, 'POINT', 2);
--- create index problem_state_easting_northing_idx on problem(state, easting, northing);
-create index place_time_position_osgb on place_time using GIST (position_osgb);
-create index place_time_position_merc on place_time using GIST (position_merc);
 
 
 -- SELECT * FROM geotable WHERE ST_DWithin(geocolumn, 'POINT(1000 1000)', 100.0);
