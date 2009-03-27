28168bea96a729135e0674882d9fc69b276d3ebd
francis <francis>
2009-03-27 14:18:57 +0000

    No need to print t each time

--- iso/tiles/Isochrones.py
+++ iso/tiles/Isochrones.py
@@ -4,7 +4,7 @@ Custom TileCache module for rendering of isochrone images based on travel time d
 Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
 Email: mike@stamen.com; WWW: http://www.mysociety.org/
 
-$Id: Isochrones.py,v 1.24 2009-03-27 13:07:35 francis Exp $
+$Id: Isochrones.py,v 1.25 2009-03-27 14:18:57 francis Exp $
 """
 import os
 import sys
@@ -130,7 +130,6 @@ def draw_tile(points, tile, log):
     # add each found data point in turn, offseting station cones by base station time
     for (x, y, t) in points:
         x, y = transform(x, y)
-        print >>log, 't is %s' % repr(t)
         draw_station(array, station_cone + t, station_mask, int(x), int(y))
 
     points_time = time.time() - points_start
