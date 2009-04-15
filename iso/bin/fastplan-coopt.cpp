a00f8d0e3205b45344b04361c00bd158e92539ed
francis <francis>
2009-04-15 18:51:32 +0000

    Turn off output buffering for C++, so progress is reflected in log files made by isodaemon.py immediately

--- iso/bin/fastplan-coopt.cpp
+++ iso/bin/fastplan-coopt.cpp
@@ -8,7 +8,7 @@
 // Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
 // Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 //
-// $Id: fastplan-coopt.cpp,v 1.8 2009-04-14 18:03:35 francis Exp $
+// $Id: fastplan-coopt.cpp,v 1.9 2009-04-15 18:51:32 francis Exp $
 //
 
 // Example one off runs (the EOF from stdin will make the program exit after one command)
@@ -29,6 +29,10 @@ int main(int argc, char * argv[]) {
         return 1;
     }
     std::string fastindexprefix = argv[1];
+
+    // Turn off buffering, so progress goes to parent isodaemon.py quickly
+    setbuf(stderr, NULL);
+    setbuf(stdout, NULL);
 
     // Load timetables
     PerformanceMonitor pm(stdout); // output everything to stdout except errors, so easier for Python script
