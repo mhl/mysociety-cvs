8867cbcb28616e2654786183fe7815eee9cf1491
francis <francis>
2009-04-22 13:17:09 +0000

    Make database ready for schema comparing

--- iso/db/schema.sql
+++ iso/db/schema.sql
@@ -4,10 +4,10 @@
 -- Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
 -- Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: schema.sql,v 1.17 2009-04-21 09:44:48 francis Exp $
+-- $Id: schema.sql,v 1.18 2009-04-22 13:17:09 francis Exp $
 --
 
--- The following must be done first to set up PostGIS, as user Postgres:
+-- The following must be done first to set up PostGIS, as user "postgres":
 
 -- CREATE FUNCTION plpgsql_call_handler()
 --         RETURNS OPAQUE AS '$libdir/plpgsql' LANGUAGE 'C';
@@ -15,10 +15,10 @@
 --     LANCOMPILER 'PL/pgSQL';
 
 -- Creating geometry_columns and spatial_ref_sys tables and populating them:
--- /usr/share/postgresql-8.3-postgis/lwpostgis.sql
--- /usr/share/postgresql-8.3-postgis/spatial_ref_sys.sql
+-- PSQL-SCHEMA-COMPARE-INCLUDE: /usr/share/postgresql-8.3-postgis/lwpostgis.sql
+-- PSQL-SCHEMA-COMPARE-INCLUDE: /usr/share/postgresql-8.3-postgis/spatial_ref_sys.sql
 
--- The following must all be done also as the postgres database user,
+-- The following must all be done also as the "postgres" database user,
 -- because the geometry_columns can't be modified otherwise.
 
 -- Every station
@@ -34,9 +34,10 @@ create unique index station_text_id_idx on station(text_id);
 create index station_minimum_zoom_idx on station(minimum_zoom);
 create index station_connectedness_idx on station(connectedness);
 -- SRID 27700 = OSGB 1936 / British National Grid
-select AddGeometryColumn('', 'station', 'position_osgb', 27700, 'POINT', 2);
+select AddGeometryColumn('', 'station', 'position_osgb', 27700, 'POINT', 2) into temp result_add_position_osgb;
 -- SRID 900913 = Spherical mercator
-select AddGeometryColumn('', 'station', 'position_merc', 900913, 'POINT', 2);
+select AddGeometryColumn('', 'station', 'position_merc', 900913, 'POINT', 2) into temp result_add_position_merc;
+-- (the "into temp" stuff above serves to suppress the output of the return value of the "select AddGeometryColumn")
 create index station_position_merc on station using GIST (position_merc);
 
 -- Represents a map made by the user
@@ -62,19 +63,11 @@ create table map (
     target_date date not null
 );
 
+-- We grant privileges back to the actual user who is using the database.
+-- Shame it had to all be made by user "postgres", see top of file.
 grant all on table station to col;
 grant all on table map to col;
 grant all on table map_id_seq to col;
-
-
--- SELECT * FROM geotable WHERE ST_DWithin(geocolumn, 'POINT(1000 1000)', 100.0);
--- SELECT road_id, AsText(road_geom) AS geom, road_name FROM roads; 
--- col=# insert into place_time values (1, 39, ST_SetSRID(GeomFromText('POINT(415431 132508)'), 27700));
-
--- col=# select *, AsText(position), AsText(ST_Transform(position, 4326)) from place_time;
--- col=# select AsText(ST_Transform(ST_SetSRID(GeomFromText('POINT(-1.78103 51.09168)'), 4326) , 27700));
--- http://www.nabble.com/SRID-tranformations-(OSGB-1936)-td20096957.html
-
--- insert into map values (1, now(), 'new', null, 5157, 540, 0, '2008-10-07');
---  340002053CR is 5157 
+grant all on table spatial_ref_sys to col;
+grant all on table geometry_columns to col;
 
