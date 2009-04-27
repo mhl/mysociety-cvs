12bd30d176f1c04940c843607f1cf8de2581267a
migurski <migurski>
2009-04-27 23:06:10 +0000

    Anyway

--- iso/tiles/scenicness-vrt.py
+++ iso/tiles/scenicness-vrt.py
@@ -1,9 +1,12 @@
 import sys
-import urllib
 import csv
+import urllib
+import pyproj
 
 if __name__ == '__main__':
 
+    GYM = pyproj.Proj(proj='merc', a=6378137, b=6378137, lat_ts=0.0, lon_0=0.0, x_0=0.0, y_0=0, k=1.0, units='m', nadgrids=None, no_defs=True)
+    
     try:
         csvfilename, vrtfilename = sys.argv[1:]
         csvfile, vrtfile = open(csvfilename, 'w'), open(vrtfilename, 'w')
@@ -17,7 +20,8 @@ if __name__ == '__main__':
         print >> vrtfile, '    <OGRVRTLayer name="scenic">'
         print >> vrtfile, '        <SrcDataSource>%s</SrcDataSource>' % csvfilename
         print >> vrtfile, '        <GeometryType>wkbPoint</GeometryType>'
-        print >> vrtfile, '        <GeometryField encoding="PointFromColumns" x="Longitude" y="Latitude" z="Rating"/>'
+        print >> vrtfile, '        <GeometryField encoding="PointFromColumns" x="Easting" y="Northing" z="Rating"/>'
+        print >> vrtfile, '        <LayerSRS>%s</LayerSRS>' % GYM.srs.replace('"', '&quot;')
         print >> vrtfile, '    </OGRVRTLayer>'
         print >> vrtfile, '</OGRVRTDataSource>'
 
@@ -25,7 +29,8 @@ if __name__ == '__main__':
         input = csv.DictReader(src, dialect='excel-tab')
         output = csv.writer(csvfile)
         
-        print >> csvfile, 'Latitude,Longitude,Rating'
+        print >> csvfile, 'Easting,Northing,Rating'
         
         for row in input:
-            output.writerow((row['Lat'], row['Lon'], row['Rating']))
+            x, y = GYM(row['Lon'], row['Lat'])
+            output.writerow((x, y, row['Rating']))
