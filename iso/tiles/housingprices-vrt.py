2f5e7c8e7bff77e9221fa2464925e721375010ee
migurski <migurski>
2009-05-06 01:18:19 +0000

    try/except, the lazy man's bugfix

--- iso/tiles/housingprices-vrt.py
+++ iso/tiles/housingprices-vrt.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
         print >> vrtfile, '        <LayerSRS>%s</LayerSRS>' % GYM.srs.replace('"', '&quot;')
         print >> vrtfile, '    </OGRVRTLayer>'
         print >> vrtfile, '</OGRVRTDataSource>'
+        vrtfile.close()
 
         src = open('ppi2317715_incl_addressesXTN.csv', 'r')
         input = csv.reader(src)
@@ -35,26 +36,44 @@ if __name__ == '__main__':
         min_date = datetime.date(2008, 1, 1)
         date_pat = re.compile(r'^(\d+)/(\d+)/(\d\d\d\d)$')
         location_pat = re.compile(r'\blocation="(-?\d+\.\d+),(-?\d+\.\d+)"')
-        postcodes = {}
+        postcodes, failed_postcodes = {}, {}
         
         for row in input:
-            amount, date, postcode = row[:3]
-            date_match = date_pat.match(date)
-            
-            if date_match:
-                date = datetime.date(*[int(date_match.group(i)) for i in (3, 2, 1)])
+            try:
+                amount, date, postcode = row[:3]
+                date_match = date_pat.match(date)
                 
-                if date < min_date:
-                    continue
-                
-                if postcode not in postcodes:
-                    places_xml = urllib.urlopen('http://locog.stamen.com/~migurski/data/www/places.php?format=xml&q=' + urllib.quote_plus(postcode)).read()
-                    places_match = location_pat.search(places_xml)
+                if date_match:
+                    date = datetime.date(*[int(date_match.group(i)) for i in (3, 2, 1)])
+                    
+                    if date < min_date:
+                        continue
+    
+                    if postcode in failed_postcodes:
+                        continue
+                    
+                    if postcode not in postcodes:
+                        places_xml = urllib.urlopen('http://locog.stamen.com/~migurski/data/www/places.php?format=xml&q=' + urllib.quote_plus(postcode)).read()
+                        places_match = location_pat.search(places_xml)
+    
+                        if places_match:
+                            latitude, longitude = [float(places_match.group(i)) for i in (1, 2)]
+                            postcodes[postcode] = latitude, longitude
+    
+                        else:
+                            failed_postcodes[postcode] = True
+                            
+                    if postcode not in postcodes:
+                        # still?
+                        continue
+                    
+                    lat, lon = postcodes[postcode]
+                    x, y = GYM(lon, lat)
+                    output.writerow((x, y, amount))
+    
+                    print >> sys.stderr, date, postcode, amount, lat, lon
+            except KeyboardInterrupt:
+                raise
 
-                    if places_match:
-                        latitude, longitude = [float(places_match.group(i)) for i in (1, 2)]
-                        postcodes[postcode] = latitude, longitude
-                        
-                lat, lon = postcodes[postcode]
-                x, y = GYM(lon, lat)
-                output.writerow((x, y, amount))
+            except:
+                pass
