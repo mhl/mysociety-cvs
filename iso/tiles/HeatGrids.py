fa9b42e1c8fbddeabd0020daa93b1e17c577f50c
francis <francis>
2009-05-12 15:25:28 +0000

    Reorganise so shared code is shared.

--- iso/tiles/HeatGrids.py
+++ iso/tiles/HeatGrids.py
@@ -4,7 +4,7 @@ Custom TileCache module for rendering of heat grids based on GDAL VRT files.
 Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
 Email: mike@stamen.com; WWW: http://www.mysociety.org/
 
-$Id: HeatGrids.py,v 1.12 2009-05-11 23:31:17 francis Exp $
+$Id: HeatGrids.py,v 1.13 2009-05-12 15:25:28 francis Exp $
 """
 import os
 import sys
@@ -20,13 +20,12 @@ import PIL.Image, PIL.ImageOps
 import TileCache
 import osgeo.gdal as gdal
 
-class ScenicLayer(TileCache.Layer.MetaLayer):
-
+class GenericHeatGridLayer(TileCache.Layer.MetaLayer):
     config_properties = [
       {'name': 'datafile', 'description': 'VRT datafile'},
       {'name': 'iso_tile_log', 'description': 'Log file, or none for none'},
     ] + TileCache.Layer.MetaLayer.config_properties 
-    
+
     def __init__(self, name, datafile=None, iso_tile_log=None, **kwargs):
         """ call super.__init__, store some other details,
             and make a permanent connection to the database
@@ -35,17 +34,18 @@ class ScenicLayer(TileCache.Layer.MetaLayer):
         self.iso_tile_log = iso_tile_log
 
         TileCache.Layer.MetaLayer.__init__(self, name, **kwargs)
-    
-    def renderTile(self, tile, force=None):
+
+    def renderTile(self, tile):
         """
         """
         # open a log file, or not, either way is cool
         log = self.iso_tile_log and open(self.iso_tile_log, 'a') or None
         
-        radius = 10000
+        radius = self.get_radius()
         datafile = self.datafile
         xmin, ymin, xmax, ymax = tile.bounds()
         width, height = tile.size()
+        layer_name = self.layer_name()
         
         # grab points data
         print >> log, (xmin, ymin, xmax, ymax), (width, height)
@@ -56,9 +56,9 @@ class ScenicLayer(TileCache.Layer.MetaLayer):
         where_clause = """-where "Easting >= %d and Northing >= %d and Easting <= %d and Northing <= %d" """ % (int(xmin - radius), int(ymin - radius), int(xmax + radius), int(ymax + radius))
 
         # render an image
-        cmd = '/usr/bin/gdal_grid -a invdist:power=2.0:smoothing=2.0:radius1=%(radius)d:radius2=%(radius)d ' % locals() \
-            + '-txe %(xmin)f %(xmax)f -tye %(ymin)f %(ymax)f -outsize %(width)d %(height)d ' % locals() \
-            + '%(where_clause)s -l scenic -of GTiff -ot Float32 %(datafile)s %(filename)s' % locals()
+        cmd = self.get_gdal_command(locals())
+        cmd += '-txe %(xmin)f %(xmax)f -tye %(ymin)f %(ymax)f -outsize %(width)d %(height)d ' % locals() \
+            + '%(where_clause)s -l %(layer_name)s -of GTiff -ot Float32 %(datafile)s %(filename)s' % locals()
 
         print >> log, cmd
         
@@ -81,81 +81,10 @@ class ScenicLayer(TileCache.Layer.MetaLayer):
         
         cell = numpy.fromstring(data, dtype=numpy.float32).reshape(rows, cols)
 
-        # flip it, because GDAL gives it to us upside-down
-        image = arr2img(cell * 10)
-        image = PIL.ImageOps.flip(image)
-        
-        os.unlink(filename)
-        
-        tile.data = img2str(image, self.extension)
-
-        return tile.data
-
-class HousingLayer(TileCache.Layer.MetaLayer):
-
-    config_properties = [
-      {'name': 'datafile', 'description': 'VRT datafile'},
-      {'name': 'iso_tile_log', 'description': 'Log file, or none for none'},
-    ] + TileCache.Layer.MetaLayer.config_properties 
-    
-    def __init__(self, name, datafile=None, iso_tile_log=None, **kwargs):
-        """ call super.__init__, store some other details,
-            and make a permanent connection to the database
-        """
-        self.datafile = datafile
-        self.iso_tile_log = iso_tile_log
-
-        TileCache.Layer.MetaLayer.__init__(self, name, **kwargs)
-    
-    def renderTile(self, tile, force=None):
-        """
-        """
-        # open a log file, or not, either way is cool
-        log = self.iso_tile_log and open(self.iso_tile_log, 'a') or None
-        
-        radius = 10000
-        datafile = self.datafile
-        xmin, ymin, xmax, ymax = tile.bounds()
-        width, height = tile.size()
-        
-        # grab points data
-        print >> log, (xmin, ymin, xmax, ymax), (width, height)
-        
-        (handle, filename) = tempfile.mkstemp(suffix='.tif', prefix='scenic-grid-')
-        os.close(handle)
-        
-        where_clause = """-where "Easting > %d and Northing > %d and Easting < %d and Northing < %d" """ % (int(xmin - radius), int(ymin - radius), int(xmax + radius), int(ymax + radius))
-
-        # render an image
-        cmd = '/usr/bin/gdal_grid -a average:radius1=%(radius)d:radius2=%(radius)d ' % locals() \
-            + '-txe %(xmin)f %(xmax)f -tye %(ymin)f %(ymax)f -outsize %(width)d %(height)d ' % locals() \
-            + '%(where_clause)s -l housingprices -of GTiff -ot Float32 %(datafile)s %(filename)s' % locals()
-
-        print >> log, cmd
-        
-        status, output = commands.getstatusoutput(cmd)
-
-        print >> log, "gdal_grid output status", status
-        
-        if status != 0:
-            raise Exception("Got error code from GDAL " + str(status) + " message " + output)
-        
-        heat = gdal.Open(filename)
-        band = heat.GetRasterBand(1)
-        cols, rows = heat.RasterXSize, heat.RasterYSize
-        
-        print >> log, "raster cols, rows", cols, rows
-        
-        data = band.ReadRaster(0, 0, cols, rows, buf_type=gdal.GDT_Float32)
-        
-        print >> log, "start of data", repr(data[:16]), struct.unpack('ffff', data[:16])
-        
-        cell = numpy.fromstring(data, dtype=numpy.float32).reshape(rows, cols)
-        
         print >> log, "min, max", numpy.min(cell), '-', numpy.max(cell)
 
         # flip it, because GDAL gives it to us upside-down
-        image = arr2img32(cell)
+        image = self.convert_cell_to_image(cell)
         image = PIL.ImageOps.flip(image)
         
         os.unlink(filename)
@@ -163,6 +92,36 @@ class HousingLayer(TileCache.Layer.MetaLayer):
         tile.data = img2str(image, self.extension)
 
         return tile.data
+ 
+class ScenicLayer(GenericHeatGridLayer):
+    def get_gdal_command(self, params):
+        cmd = '/usr/bin/gdal_grid -a invdist:power=2.0:smoothing=2.0:radius1=%(radius)d:radius2=%(radius)d ' % params
+        return cmd
+
+    def convert_cell_to_image(self, cell):
+        image = arr2img(cell * 10)
+        return image
+
+    def layer_name(self):
+        return "scenic"
+
+    def get_radius(self):
+        return 10000
+
+class HousingLayer(GenericHeatGridLayer):
+    def get_gdal_command(self, params):
+        cmd = '/usr/bin/gdal_grid -a average:radius1=%(radius)d:radius2=%(radius)d ' % params
+        return cmd
+
+    def convert_cell_to_image(self, cell):
+        image = arr2img32(cell)
+        return image
+
+    def layer_name(self):
+        return "housingprices"
+
+    def get_radius(self):
+        return 10000
 
 def arr2img(ar):
     """ Convert Numeric.array to PIL.Image.
