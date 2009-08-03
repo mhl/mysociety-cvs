6517aee41809c0e772cacad5424f1313276b6158
richard <richard>
2009-08-03 14:34:29 +0000

    plugins and themes

---
+++ ms/web/wordpress_2_8_2/wp-content/themes/default/fullpage.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Full width
+*/
+?>
+
+<?php get_header(); ?>
+
+	<h1><?php the_title(); ?></h1>
+	<div class="contentfull">
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+
+				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+
+		<?php endwhile; endif; ?>
+	
+	
+		<?php edit_post_link('Edit this entry.', '<p class="editlink">', '</p>'); ?>
+
+	</div>
+
+<?php include (TEMPLATEPATH . '/catposts.php'); ?>
+<?php get_footer(); ?>
