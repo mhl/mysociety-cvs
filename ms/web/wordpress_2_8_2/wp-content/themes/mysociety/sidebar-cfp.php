e0e7e78813a784e9f826aecdb6d1942fd436d687
matthew <matthew>
2009-10-20 09:36:50 +0000

    Factor out CFP sidebar to proper WP file.

---
+++ ms/web/wordpress_2_8_2/wp-content/themes/mysociety/sidebar-cfp.php
@@ -0,0 +1,46 @@
+<?php
+
+if ($is_idea) {
+    $add_link = '/call-for-proposals-2009/';
+    $add_text = 'Add an idea&hellip;';
+    $view_link = '/category/proposal-submissions-2009/';
+} elseif ($is_cee_cfp) {
+    $add_link = '/cfp/';
+    $view_link = '/cfp/view/';
+    $add_text = 'Submit a proposal';
+}
+
+?>
+
+<div class="contentnarrow right">
+    <p>
+    <a class="linkbutton" href="<?=$add_link?>">
+        <span class="left">&nbsp;</span>
+        <span class="middle"><?=$add_text?></span>
+        <span class="right">&nbsp;</span>
+    </a>
+    <br class="clear"/>
+    </p>
+
+    <h3>Ideas so far</h3>
+    <!--Recent posts-->
+    <ul>
+<?php
+
+if ($is_idea) {
+    query_posts('category_name=proposal-submissions-2009&showposts=10');
+} elseif ($is_cee_cfp) {
+    query_posts('showposts=10');
+}
+
+while (have_posts()) : the_post(); ?>
+        <li>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </li>
+<?php endwhile; ?>
+    </ul>
+
+    <a href="<?=$view_link?>">View all ideas &raquo;</a>
+
+</div>
+
