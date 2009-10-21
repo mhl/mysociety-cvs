951797e51d9f6dd0b953b15f2e858ef0e58c5606
louise <louise>
2009-10-21 15:57:08 +0000

    Moving MockQuery to lib dir where it can be accessed by other app tests

--- perllib/mySociety/MockQuery.pm
+++ perllib/mySociety/MockQuery.pm
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 #
 # MockQuery.pm:
-# Mock query to support tests for the Page functions
+# Mock query to support tests
 #
 #  Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
 # Email: louise@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: MockQuery.pm,v 1.2 2009-09-15 13:55:17 louise Exp $
+# $Id: MockQuery.pm,v 1.1 2009-10-21 15:58:24 louise Exp $
 #
 
 
