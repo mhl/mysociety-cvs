66de7ace811bafddf10589c7ad6cd12bc4edbdc4
matthew <matthew>
2010-04-16 15:19:45 +0000

    Make required fields required.

--- ms/includer/quote.php
+++ ms/includer/quote.php
@@ -2,6 +2,8 @@
 
 require_once dirname(__FILE__) . "/../../phplib/evel.php";
 
+$sent = false;
+
 if (isset($_POST['_is_postback']) && $_POST['_is_postback'] && strtolower($_POST['theWord']) == 'tangent') {
 
     $to = OPTION_QUOTE_EMAIL;
@@ -9,25 +11,33 @@ if (isset($_POST['_is_postback']) && $_POST['_is_postback'] && strtolower($_POST
     $from_name = $_POST['txtName'];
     $area = $_POST['area'];
     $org = $_POST['txtOrganisation'];
+    $info = $POST['information'];
 
-    $subject = "[mySociety Quote Request] $area";
-    if ($org) {
-        $subject .= " - $org";
+    if ($from_name && $from_email && $area && $info) {
+
+        $subject = "[mySociety Quote Request] $area";
+        if ($org) {
+            $subject .= " - $org";
+        }
+
+        $body = "$info\n-- \nForm submission from the mySociety website\n";
+
+        evel_send(array(
+            '_body_' => $body,
+            'To' => $to,
+            'From' => array($from_email, $from_name),
+            'Subject' => $subject,
+        ), $to);
+        $sent = true;
+
+        print '<p>Thanks, your request has been sent and someone will get back to you soon.
+        <br /><a href="/">Return to the mySociety homepage</a></p>';
+    } else {
+        print '<p>Please check and fill in the required fields below.</p>';
     }
+}
 
-    $body = $_POST['information'] . "\n-- \nForm submission from the mySociety website\n";
-
-    evel_send(array(
-        '_body_' => $body,
-        'To' => $to,
-        'From' => array($from_email, $from_name),
-        'Subject' => $subject,
-    ), $to);
-
-    print '<p>Thanks, your request has been sent and someone will get back to you soon.
-<br /><a href="/">Return to the mySociety homepage</a></p>';
-
-} else {
+if (!$sent) {
 
 ?>
     <form method="POST">
@@ -35,18 +45,18 @@ if (isset($_POST['_is_postback']) && $_POST['_is_postback'] && strtolower($_POST
         <ul class="nobullets form">
             <li>
                 <label for="txtName">Name *</label>
-                <input type="text" class="textbox large" id="txtName" name="txtName" />
+                <input type="text" class="textbox large" id="txtName" name="txtName" value="<?=htmlspecialchars($_POST['txtName'])?>" />
             </li>
             <li>
                 <label for="txtEmail">Email *</label>
-                <input type="text" class="textbox large" id="txtEmail" name="txtEmail" />
+                <input type="text" class="textbox large" id="txtEmail" name="txtEmail" value="<?=htmlspecialchars($_POST['txtEmail'])?>" />
             </li>
             <li>
                 <label for="txtOrganisation">Organisation</label>
-                <input type="text" class="textbox large" id="txtOrganisation" name="txtOrganisation" />
+                <input type="text" class="textbox large" id="txtOrganisation" name="txtOrganisation" value="<?=htmlspecialchars($_POST['txtOrganisation'])?>" />
             </li>
             <li>
-                <label for="quote_area">Area of interest</label>
+                <label for="quote_area">Area of interest *</label>
                 <select name="area" id="quote_area">
                     <option>Petitions
                     <option>FixMyStreet
@@ -56,14 +66,14 @@ if (isset($_POST['_is_postback']) && $_POST['_is_postback'] && strtolower($_POST
                     </select>
                 </li>
                 <li>
-                    <label for="quote_information">Details of your enquiry</label>
-                    <textarea id="quote_information" name="information" cols=40 rows=10></textarea>
+                    <label for="quote_information">Details of your enquiry *</label>
+                    <textarea id="quote_information" name="information" cols=40 rows=10><?=htmlspecialchars($_POST['information'])?></textarea>
                     <br /><small>e.g. what you are specifically interested in, what is your budget, timescale, etc.</small>
                 </li>
                 <li>
-                  <p>Please enter the word &ldquo;tangent&rdquo; into the box below so we know that you&rsquo;re a real person, thanks</p>
-                    <label for="theWord">Enter "tangent"</label>
-                    <input type="text" class="textbox large" id="theWord" name="theWord" />
+                  <p>Please enter the word &ldquo;tangent&rdquo; into the box below so we know that you&rsquo;re a real person, thanks.</p>
+                    <label for="theWord">Enter &ldquo;tangent&rdquo;</label>
+                    <input type="text" class="textbox large" id="theWord" name="theWord" value="<?=htmlspecialchars($_POST['theWord'])?>" />
                 </li>
             </ul>
             <div class="buttons">
