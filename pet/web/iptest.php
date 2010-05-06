adc9d95ef3de2d018c5dc2d78cf07dd913a4639c
matthew <matthew>
2010-05-06 14:04:54 +0000

    Moving petitions to git.

--- pet/web/iptest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-print "<p>REMOTE_ADDR: " . htmlspecialchars($_SERVER['REMOTE_ADDR']) . "</p>";
-print "<p>HTTP_X_FORWARDED_FOR: " . htmlspecialchars($_SERVER['HTTP_X_FORWARDED_FOR']) . "</p>";
-
