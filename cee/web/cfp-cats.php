9ca4f851c0b2681817d42384689174ae5af01ff4
matthew <matthew>
2011-07-25 18:57:10 +0000

    Removing cee from CVS.

--- cee/web/cfp-cats.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-# Copy of the lines from WP main index.php, so it can be in subdirectory
-define('WP_USE_THEMES', true);
-include_once 'cfp/wp-config.php';
-
-print wp_list_categories('title_li=&show_count=0');
-
