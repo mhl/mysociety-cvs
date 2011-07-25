5a241501c0179076848f25c5ee432541255c0254
matthew <matthew>
2011-07-25 23:36:58 +0000

    Removing www. and secure. mysociety.org from CVS.

--- secure/web/donate/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$file = file_get_contents('http://www.mysociety.org/donate/');
-$file = preg_replace('#href="/#', 'href="http://www.mysociety.org/', $file);
-$file = preg_replace('#(rel="stylesheet" href=")http://www.mysociety.org/#', '$1/nonsecure/', $file);
-$file = preg_replace('#src="/#', 'src="/nonsecure/', $file);
-$file = preg_replace('#<!-- Piwik.*?/Piwik -->#s', '', $file);
-print $file;
-
